dd0c71431daaf151d49bd041133ca74bf1410d80
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-07-18 19:26:50 +0100

    super admin side nav

---
+++ src/app/guide/superadminsidenav/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import SuperAdminSideNav from "~/components/common/SuperAdminSideNav"
+
+const SuperAdmin: React.FC = () => {
+    return (
+        <>
+        <SuperAdminSideNav />
+        </>
+    )
+}
+
+export default SuperAdmin
