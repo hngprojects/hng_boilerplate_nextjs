2b83ab80dc7c5c537ae8b8344c641cbe7df23854
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-07-18 21:00:52 +0100

    feat: settings layout

--- src/components/common/settings/sidebar.tsx
+++ src/components/common/settings/sidebar.tsx
@@ -73,19 +73,19 @@ const SettingsSidebar = () => {
     setActiveLink(currentPath);
   }, [currentPath]);
   return (
-    <section className="bg-[#FAFAFA] z-[50] w-[0px] md:w-[96px] min-[1140px]:w-[270px] hover:w-[270px] hover:p-4 transition-all duration-300 py-6 min-[1140px]:p-4 hidden md:flex flex-col gap-y-4 items-center  fixed h-screen left-0 top-0 overflow-hidden group select-none rounded-t-[16px]">
-      <div className="items-center">
-        <button
-          className={cn(
-            "flex gap-x-3 py-2 px-3 h-[52px] font-medium text-base transition-colors duration-300 cursor-pointer rounded-md justify-start items-start",
-          )}
-          onClick={() => router.back()}
-        >
-          <ChevronLeft />
-          <h2>Settings</h2>
-        </button>
-      </div>
+    <section className="bg-[#FAFAFA] z-[50] w-[0px] md:w-[96px] min-[1140px]:w-[270px] hover:w-[270px] hover:p-4 transition-all duration-300 py-6 min-[1140px]:p-4 hidden md:flex flex-col gap-y-4 items-center h-screen left-0 top-0 overflow-hidden group select-none rounded-t-[16px]">
       <ul className="flex flex-col gap-y-4 pt-8">
+        <div className="items-center">
+          <button
+            className={cn(
+              "flex gap-x-3 py-2 px-3 h-[52px] font-medium transition-colors duration-300 cursor-pointer rounded-md justify-center items-center text-2xl",
+            )}
+            onClick={() => router.back()}
+          >
+            <ChevronLeft size={24} />
+            <h2>Settings</h2>
+          </button>
+        </div>
         {Settings_Sidebar_Links.map((link) => (
           <Link
             href={`/settings/${link.link}`}
@@ -100,8 +100,8 @@ const SettingsSidebar = () => {
             className={cn(
               "flex items-center gap-x-3 py-2 px-3 h-[52px] text-[#3a3a3a] font-medium text-base transition-colors duration-300 cursor-pointer rounded-md",
               activeLink === link.link
-                ? "bg-primary text-white rounded outline-none"
-                : "hover:bg-primary/30 focus-visible:bg-black/5 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-light",
+                ? "bg-[#f97316] text-white rounded outline-none"
+                : "hover:bg-[#f97316]/30 focus-visible:bg-black/5 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-light",
             )}
           >
             <link.icon size={24} aria-hidden />
