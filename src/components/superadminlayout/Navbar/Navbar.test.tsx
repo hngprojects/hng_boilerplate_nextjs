0a0f53d4aedc6ae2d2abf8a84987943356e530eb
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2024-07-18 17:36:05 +0100

    feat: tests created

--- src/components/superadminlayout/Navbar/Navbar.test.tsx
+++ src/components/superadminlayout/Navbar/Navbar.test.tsx
@@ -3,11 +3,48 @@ import { render } from "@testing-library/react";
 import Navbar from "./index";
 
 describe("component rendering tests", () => {
-  it("should render correctly", () => {
+  it("render help icon", () => {
+    expect.assertions(1);
+    const utils = render(<Navbar />);
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(utils.getByTestId("help")).toBeTruthy();
+  });
+  it("render bell icon", () => {
     expect.assertions(1);
 
-    render(<Navbar />);
+    const utils = render(<Navbar />);
 
-    expect(true).toBeTruthy();
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(utils.getByTestId("bell")).toBeTruthy();
+  });
+  it("render chevron down icon", () => {
+    expect.assertions(1);
+
+    const utils = render(<Navbar />);
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(utils.getByTestId("chevronDown")).toBeTruthy();
+  });
+  it("input filed icon", () => {
+    expect.assertions(1);
+    const utils = render(<Navbar />);
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(utils.getByTestId("search")).toBeTruthy();
+  });
+  it("input field has placeholder", () => {
+    expect.assertions(1);
+    const utils = render(<Navbar />);
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(utils.getByPlaceholderText("Search option...")).toBeTruthy();
+  });
+
+  it("input field renders", () => {
+    expect.assertions(1);
+    const utils = render(<Navbar />);
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(utils.getByTestId("input")).toBeTruthy();
   });
 });
--- src/components/superadminlayout/Navbar/index.tsx
+++ src/components/superadminlayout/Navbar/index.tsx
@@ -6,26 +6,39 @@ const index = () => {
   return (
     <nav className="flex justify-between items-center gap-2 bg-[#FDFDFD] px-[10px] py-[14px]">
       <div className="flex justify-between items-center gap-2 px-3 h-10 border border-[#E4E4E7] rounded-[6px] text-sm placeholder:text-sm font-normal">
-        <SearchIcon className="h-4 w-4 text-neutral-dark-2" />
+        <SearchIcon
+          data-testid="search"
+          className="h-4 w-4 text-neutral-dark-2"
+        />
         <input
           className="border-none outline-none ring-0 w-full h-full text-neutral-dark-2 placeholder:text-neutral-dark-1"
           placeholder="Search option..."
+          data-testid="input"
         />
       </div>
       <div className="w-full max-w-[152px] flex justify-between items-center gap-1">
         <div className="relative">
-          <BellIcon className="text-neutral-dark-2 h-6 w-6" />
+          <BellIcon
+            data-testid="bell"
+            className="text-neutral-dark-2 h-6 w-6"
+          />
           <span className="h-[6px] w-[6px] rounded-full bg-error absolute top-0 right-1"></span>
         </div>
         <div>
-          <HelpCircle className="text-neutral-dark-2 h-6 w-6" />
+          <HelpCircle
+            data-testid="help"
+            className="text-neutral-dark-2 h-6 w-6"
+          />
         </div>
         <div className="flex justify-between items-center gap-2 max-w-[64px] w-full">
           <Avatar className="h-10 w-10">
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <ChevronDown className="text-neutral-dark-1 h-5 2-5" />
+          <ChevronDown
+            data-testid="chevronDown"
+            className="text-neutral-dark-1 h-5 2-5"
+          />
         </div>
       </div>
     </nav>
