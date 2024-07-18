fa796c25b5727e19cf9da8f0fac8d6b7a2b9aae8
itsjustonyii <onyinyechiokeke733@gmail.com>
2024-07-19 00:20:35 +0100

    feat: create delete member modal

--- src/hooks/useDeleteModal.tsx
+++ src/hooks/useDeleteModal.tsx
@@ -1,17 +1,11 @@
 import { useState } from 'react';
 
 export const useDeleteModal = () => {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-
-  const openDeleteModal = () => {
-    console.log('Opening modal');
-    setIsVisible(true);
-  };
+  const [isVisible, setIsVisible] = useState<boolean>(true);
 
   const closeDeleteModal = () => {
-    console.log('Closing modal');
-    setIsVisible(false);
+    setIsVisible(!isVisible);
   };
 
-  return { isVisible, closeDeleteModal, openDeleteModal };
+  return { isVisible, closeDeleteModal };
 };
