ea7f7bb0836f4b2d464ca16515fd946fa1209da2
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-18 19:14:12 +0100

    feat: add badge style

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -7,11 +7,33 @@
 import BadgeInterface from "./BadgeInterface";
 
 const Badge = ({ label, icon, variant }: BadgeInterface) => {
+  const badgeTextVariants = {
+    default: "text-[#344054]",
+    primary: "text-[#B15210]",
+    success: "text-[#0B7C17]",
+    error: "text-[#A50909]",
+  };
+  const badgeBackgroundVariants = {
+    default: "bg-[#F2F4F7]",
+    primary: "bg-[#FFFAF7]",
+    success: "bg-[#E7F7E9]",
+    error: "bg-[#FDE7E7]",
+  };
+
+  const smallStyling = "py-[2px] pr-[8px] pl-[6px] leading-[18px]";
+  const mediumStyling =
+    "md:py-[2px] md:pr-[10px] md:pl-[8px] md:text-[0.875rem]";
+  const largeStyling =
+    "lg:py-[4px] lg:pr-[12px] lg:pl-[10px] lg:leading-[20px]";
+  const commonStyling =
+    "capitalize flex gap-[6px] items-center rounded-[16px] max-w-max  font-[500] text-[0.75rem]";
+
   return (
-    <div>
+    <div
+      className={`${badgeTextVariants[variant]} ${badgeBackgroundVariants[variant]} ${commonStyling} ${smallStyling} ${mediumStyling} ${largeStyling} `}
+    >
+      <p>{icon && icon}</p>
       <p>{label}</p>
-      <p>{icon}</p>
-      <p>{variant}</p>
     </div>
   );
 };
