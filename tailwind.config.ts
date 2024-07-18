c15a1af96c20f2b5d43a81dd5694073a0cfe3981
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-07-18 20:32:08 +0100

    chore: app theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,8 @@ const config = {
     },
     extend: {
       colors: {
+        "app-theme": "#F97316",
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
