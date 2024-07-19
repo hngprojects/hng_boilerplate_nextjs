6839dac5081a6f3172ca90b144f6c8c821a748bd
Russell Oje <russelloje@gmail.com>
2024-07-19 13:35:45 +0100

    Template for Email Welcome with image

---
+++ src/email/templates/welcome/with-image.tsx
@@ -0,0 +1,151 @@
+import {
+  Button,
+  Font,
+  Head,
+  Html,
+  Img,
+  Preview,
+  Text,
+} from "@react-email/components";
+
+import Star from "~/email/templates/_components/Star";
+import Tailwindwrapper from "~/email/templates/_components/tailwindWrapper";
+
+interface IProperties {
+  name: string;
+  main_heading: string;
+  sub_heading: string;
+  description: string;
+  with_list: boolean;
+  list: {
+    title: string;
+    body: {
+      item: string;
+      description: string;
+    }[];
+  };
+  action_url: string;
+  app_name: string;
+}
+
+export default function Email(properties: IProperties) {
+  const {
+    main_heading,
+    sub_heading,
+    name,
+    description,
+    with_list,
+    list,
+    action_url,
+    app_name,
+  } = properties;
+
+  return (
+    <Html>
+      <Preview>{`Welcome ${name}`}</Preview>
+      <Tailwindwrapper>
+        <Head>
+          <Font
+            fontFamily="Inter"
+            fallbackFontFamily="sans-serif"
+            webFont={{
+              url: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
+              format: "woff2",
+            }}
+          />
+        </Head>
+        <section className="mx-auto flex max-w-[680px] flex-col items-center justify-center gap-10 self-stretch px-14 py-12 md:gap-14 md:py-14">
+          <div className="px-14 text-center">
+            <Img
+              src={"https://i.ibb.co/ScGZJHs/image-2-2.png"}
+              alt={main_heading}
+              className="w-full min-w-[366px]"
+            />
+          </div>
+          <header className="flex w-full flex-col items-center justify-center gap-2 md:gap-3">
+            <h4 className="my-0 text-center text-xl font-semibold text-primary md:text-2xl">
+              {main_heading}
+            </h4>
+            <h6 className="my-0 text-center text-base font-medium md:text-lg">
+              {sub_heading}
+            </h6>
+          </header>
+          <main className="flex w-full flex-col gap-7 self-stretch md:gap-8">
+            <p className="my-0 text-base font-semibold md:text-lg">
+              Hi {name},
+            </p>
+            <div className="flex flex-col gap-6 md:gap-7">
+              <p className="my-0 text-sm font-normal md:text-base">
+                {description}
+              </p>
+              {with_list && (
+                <div className="flex flex-col gap-5">
+                  <p className="my-0 text-base font-semibold">{list.title}</p>
+                  <div className="flex flex-col gap-4">
+                    {list.body.map((item, index) => (
+                      <Text
+                        key={index}
+                        className="my-0 flex items-center gap-3 leading-4 md:gap-4"
+                      >
+                        <span className="text-primary">
+                          <Star />
+                        </span>
+                        <span>
+                          <span className="font-semibold">{item.item}: </span>
+                          <span className="text-sm md:text-base">
+                            {item.description}
+                          </span>
+                        </span>
+                      </Text>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="text-center">
+              <Button
+                href={action_url}
+                className="block rounded-[8px] bg-[#F97316] px-10 py-2.5 text-base text-white sm:inline"
+              >
+                Learn more about us
+              </Button>
+            </div>
+          </main>
+          <footer className="flex w-full flex-col gap-2">
+            <Text className="my-0 text-sm font-medium">Regards,</Text>
+            <Text className="my-0 text-sm font-medium">{app_name}</Text>
+          </footer>
+        </section>
+      </Tailwindwrapper>
+    </Html>
+  );
+}
+
+Email.PreviewProps = {
+  name: "John Doe",
+  main_heading: "Welcome to Boilerplate",
+  sub_heading: "Get started with us today",
+  description:
+    "We're thrilled to have you join us. Experience quality and innovation like never before.  Our product is made to fit your needs and make your life easier.",
+  with_list: true,
+  list: {
+    title: "Here’s what you can look forward to",
+    body: [
+      {
+        item: "Exclusive Offers",
+        description:
+          "Enjoy special promotions and discounts available only to our members.",
+      },
+      {
+        item: "24/7 Support",
+        description: "To help you at any time",
+      },
+      {
+        item: "Secure and reliable",
+        description: "Platform to keep your data safe",
+      },
+    ],
+  },
+  action_url: "https://boilerplate-weblink.com",
+  app_name: "Boilerplate",
+} satisfies IProperties;
