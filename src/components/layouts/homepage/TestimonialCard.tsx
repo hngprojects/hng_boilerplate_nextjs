ce350f9c35c29f1c26f8ff24c31ea2e5197bdf84
Ayodejioladimeji <layobright11@gmail.com>
2024-07-20 00:47:25 +0100

    write tests for testimonial card

--- src/components/layouts/homepage/TestimonialCard.tsx
+++ src/components/layouts/homepage/TestimonialCard.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 import { Rating } from './svgs'
 
 interface Props {
-    image:string,
-    content:string,
-    name:string,
-    stack:string
+    image: string,
+    content: string,
+    name: string,
+    stack: string
 }
 
 const TestimonialCard = (props: Props) => {
@@ -14,10 +14,12 @@ const TestimonialCard = (props: Props) => {
         <div className="testimonial-card bg-[#ffffff] border border-primary border-solid ">
             <p>“I've been using this web hosting service for over a year and I'm really impressed with the uptime and support. The website has never gone down and the customer service is always quick to help with any issues I have. Highly recommend!”</p>
 
-            <Rating/>
+            <div data-testid="rating">
+                <Rating />
+            </div>
 
             <div className="image-box">
-                <Image src={props?.image} alt="testimonial-image" width={100} height={100}/>
+                <Image src={props?.image} alt="testimonial-image" width={100} height={100} />
             </div>
 
             <h4>{props?.name}</h4>
