46bff3675807e7f0981eea79abdb79face19040e
luqmonac56 <luqmonac56@gmail.com>
2024-07-19 16:42:41 +0100

    feat: create dashboard tabs for organization settings

---
+++ src/app/guide/dashboard-menu/page.tsx
@@ -0,0 +1,11 @@
+import DashboardTabs from "~/components/common/Dashboard/DashboardTabs";
+
+const page = () => {
+  return (
+    <div>
+      <DashboardTabs />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/common/Dashboard/DashboardTabs.tsx
@@ -0,0 +1,38 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+
+const index = () => {
+  return (
+    <div className="flex  flex-col gap-6">
+      <h1 data-testid="tab" className="text-3xl font-semibold">
+        Dashboard
+      </h1>
+      <Tabs defaultValue="overview" className="w-[400px]">
+        <TabsList data-testid="tab" className="bg-white">
+          <TabsTrigger aria-label="tab 1" role="tab" value="overview">
+            Overview
+          </TabsTrigger>
+          <TabsTrigger aria-label="tab 2" role="tab" value="analytics">
+            Analytics
+          </TabsTrigger>
+          <TabsTrigger aria-label="tab 3" role="tab" value="reports">
+            Reports
+          </TabsTrigger>
+        </TabsList>
+
+        <div className="mt-6">
+          <TabsContent role="tabpanel" value="overview">
+            Make changes to the overview here.
+          </TabsContent>
+          <TabsContent role="tabpanel" value="analytics">
+            Change your analytics content here.
+          </TabsContent>
+          <TabsContent role="tabpanel" value="reports">
+            Change your report content here.
+          </TabsContent>
+        </div>
+      </Tabs>
+    </div>
+  );
+};
+
+export default index;
