7ffe9b738e7882cc759e775eacb9af47f63646f3
Moluno-xiii <molunoprogress@gmail.com>
2024-07-19 19:29:26 +0100

    fix: correct test to ensure footer hides properly

--- src/components/layouts/CookieFooter/CookieFooter.test.tsx
+++ src/components/layouts/CookieFooter/CookieFooter.test.tsx
@@ -5,12 +5,16 @@ import CookieFooter from '.';
 describe('CookieFooter', () => {
     test('renders the required contents', () => {
         render(<CookieFooter />);
+
         const footerElement = screen.getByRole('contentinfo');
         expect(footerElement).toBeInTheDocument();
+        
         const privacyMessage = screen.getByText(/We Value your Privacy/i);
         expect(privacyMessage).toBeInTheDocument();
+        
         const cookieDescription = screen.getByText(/Our website uses cookies to enhance your browsing experience, provide personalized content, and analyze site traffic. By clicking "Accept All", you consent to our use of cookies./i);
         expect(cookieDescription).toBeInTheDocument();
+        
         const acceptButton = screen.getByText(/accept all cookies/i);
         const rejectButton = screen.getByText(/reject all/i);
         const settingsButton = screen.getByText(/cookie settings/i);
@@ -19,14 +23,18 @@ describe('CookieFooter', () => {
         expect(settingsButton).toBeInTheDocument();
     });
 
-    test('hides the footer when a button is clicked', () => {
+    test('hides the footer when any button is clicked', () => {
         render(<CookieFooter />);
         const footerElement = screen.getByRole('contentinfo');
         expect(footerElement).toBeInTheDocument();
 
         const acceptButton = screen.getByText(/accept all cookies/i);
         fireEvent.click(acceptButton);
+        expect(footerElement).not.toBeInTheDocument();
 
+        render(<CookieFooter />);
+        const rejectButton = screen.getByText(/reject all/i);
+        fireEvent.click(rejectButton);
         expect(footerElement).not.toBeInTheDocument();
     });
 });
