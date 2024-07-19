aa6b39c12feb914807d83ff0d2442df5a2db2626
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2024-07-19 22:48:39 +0100

    feat: removed extra div wrapping adminnavbar

--- src/components/superadminlayout/index.tsx
+++ src/components/superadminlayout/index.tsx
@@ -10,9 +10,7 @@ const AdminLayout: React.FC<IProperties> = ({ children }) => {
     <div className="grid min-h-screen w-full md:grid-cols-[252px_1fr] lg:grid-cols-[252px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block"></div>
       <div className="flex flex-col">
-        <div>
-          <AdminNavbar />
-        </div>
+        <AdminNavbar />
         {children}
       </div>
     </div>
