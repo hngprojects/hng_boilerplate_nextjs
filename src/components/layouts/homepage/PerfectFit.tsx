58bfdf44862a017bf9bf23ed14475c457ce61556
Ayodejioladimeji <layobright11@gmail.com>
2024-07-19 22:16:50 +0100

    Worked on Homepage

---
+++ src/components/layouts/homepage/PerfectFit.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const PerfectFit = () => {
+    return (
+        <div className="perfect-fit">
+            <div className="max-w-7xl mx-auto px-5 md:px-10 lg:px-10 xl:px-10">
+                <h1>Find The Perfect Fit</h1>
+                <p>Choose the boilerplate plan that best suits your project needs and budget. All plans include access to our comprehensive library of pre-built sections, drag-and-drop customization.</p>
+                <button>See Our Pricing Plan</button>
+            </div>
+        </div>
+    )
+}
+
+export default PerfectFit
