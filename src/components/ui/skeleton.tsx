5a0178afc555a23e984d73ae6de32e0d1c60ebf7
YeeshaDev <aishaagunbiade05@gmail.com>
2024-07-19 08:46:51 +0100

    feat/88  career card component and testing

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,15 +1,15 @@
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 
 function Skeleton({
   className,
-  ...props
+  ...properties
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
+      {...properties}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
