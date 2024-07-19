544419644f6369cfe160372143e9b0793504a048
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-19 18:39:19 +0100

    refactor: Refactor css style to be inline

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -43,29 +43,15 @@ export interface BadgeInterface {
 
 const Badge = ({ label, icon, variant }: BadgeInterface) => {
   const badgeTextVariants = {
-    default: "text-[#344054]",
-    primary: "text-[#B15210]",
-    success: "text-[#0B7C17]",
-    error: "text-[#A50909]",
+    default: "text-[#344054] bg-[#F2F4F7]",
+    primary: "text-[#B15210] bg-[#FFFAF7]",
+    success: "text-[#0B7C17] bg-[#E7F7E9]",
+    error: "text-[#A50909] bg-[#FDE7E7]",
   };
-  const badgeBackgroundVariants = {
-    default: "bg-[#F2F4F7]",
-    primary: "bg-[#FFFAF7]",
-    success: "bg-[#E7F7E9]",
-    error: "bg-[#FDE7E7]",
-  };
-
-  const smallStyling = "py-[2px] pr-[8px] pl-[6px] leading-[18px]";
-  const mediumStyling =
-    "md:py-[2px] md:pr-[10px] md:pl-[8px] md:text-[0.875rem]";
-  const largeStyling =
-    "lg:py-[4px] lg:pr-[12px] lg:pl-[10px] lg:leading-[20px]";
-  const commonStyling =
-    "capitalize flex gap-[6px] items-center rounded-[16px] max-w-max  font-[500] text-[0.75rem]";
 
   return (
     <div
-      className={`${badgeTextVariants[variant]} ${badgeBackgroundVariants[variant]} ${commonStyling} ${smallStyling} ${mediumStyling} ${largeStyling} `}
+      className={`${badgeTextVariants[variant]} py-[2px] pr-2 pl-[6px] leading-[18px] md:py-[2px] md:pr-[10px] md:pl-[8px] md:text-[0.875rem] lg:py-1 lg:pr-3 lg:pl-[10px] lg:leading-[20px] capitalize flex gap-[6px] items-center rounded-[16px] max-w-max  font-[500] text-[0.75rem]`}
     >
       <span role="img" aria-label={label}>
         {icon && icon}
