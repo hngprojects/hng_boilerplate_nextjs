24b51adf6e0a894ec72eeb12f592edfd88d2e218
Ayodejioladimeji <layobright11@gmail.com>
2024-07-19 22:16:50 +0100

    Worked on Homepage

---
+++ src/components/layouts/homepage/Hero.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Image from 'next/image'
+import React from 'react'
+import { HeroBoilerPlate, HeroChat, HeroCheckMark, HeroLine } from '~/components/svgs'
+import { Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+interface Props {
+
+}
+
+
+const Hero = (props: Props) => {
+    return (
+        <div className="hero">
+            <div className="max-w-7xl mx-auto px-5 md:px-10 lg:px-10 xl:px-10">
+                <div className="hero-content">
+                    <div className="hero-left">
+                        <h1>
+                            Focus on What Matters. We've Got the Foundation Covered.
+                        </h1>
+
+                        <div>
+                            <HeroLine />
+                        </div>
+
+                        <p>
+                            Streamline your processes with a boilerplate built for efficiency and optimal productivity.
+                        </p>
+
+                        <button className="get-started">
+                            Get Started
+                        </button>
+                    </div>
+
+
+                    <div className="hero-right">
+
+
+                        <div className="image-container">
+                            <div className="hero-checkmark">
+                                <HeroCheckMark />
+                            </div>
+
+                            <div className="hero-chat">
+                                <HeroChat />
+                            </div>
+
+                            <div className="hero-boilerplate">
+                                <HeroBoilerPlate />
+                            </div>
+
+                            <Swiper
+                                modules={[Pagination, Scrollbar, A11y, Autoplay]}
+                                spaceBetween={1}
+                                slidesPerView={1}
+                                // onSwiper={(swiper) => console.log("")}
+                                autoplay={{
+                                    delay: 3000,
+                                    disableOnInteraction: false,
+                                }}
+                                loop={true}
+                                speed={4000}
+                            >
+
+                                <SwiperSlide>
+                                    <Image src="/images/hero-image.svg" alt="Hero Image" width={100} height={100} unoptimized/>
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <Image src="/images/hero-image.svg" alt="Hero Image" width={100} height={100} unoptimized/>
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <Image src="/images/hero-image.svg" alt="Hero Image" width={100} height={100} unoptimized/>
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <Image src="/images/hero-image.svg" alt="Hero Image" width={100} height={100} unoptimized/>
+                                </SwiperSlide>
+
+                            </Swiper>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
