dd758748058d98dc92262a554f41176089020341
Jibola Paul <harjibbolar26@gmail.com>
2024-07-19 19:00:29 +0100

    feat: responsiveness for all screensize, sticky table of content

---
+++ src/app/components/TermsAndConditions/Discclaimer.tsx
@@ -0,0 +1,28 @@
+import { Disclaimers } from "~/app/constants/constant";
+import { TermsType } from "~/app/types/types";
+
+const Discclaimer = () => {
+  return (
+    <div className="self-stretch flex flex-col items-start justify-start gap-[10px]">
+      <h2 className="mt-0 self-stretch relative text-inherit font-bold font-inherit sm:text-[28px] text-2xl">
+        Disclaimers and Limitations of Liability
+      </h2>
+      <div className="self-stretch relative inline-block text-base">
+        <div className="mb-[10px]">
+          To ensure clarity regarding our responsibilities and your
+          expectations, the following disclaimers and limitations of liability
+          apply:
+        </div>
+        <ul className=" pl-[21px]">
+          {Disclaimers.map((list: TermsType) => (
+            <li className="list-disc mb-[10px]" key={list.title}>
+              <span>{list.title}</span> <span>{list.value}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Discclaimer;
---
+++ src/app/terms_and_conditions/page.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+import Breadcrumb from "../components/TermsAndConditions/Main";
+import Hero from "../components/TermsAndConditions/Hero";
+
+const TermsConditions = () => {
+  return (
+    <div>
+      <Hero />
+      <Breadcrumb />
+    </div>
+  );
+};
+
+export default TermsConditions;
