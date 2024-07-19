5bed76f85c32e39ff91935acc0b91cff0cc43f7c
Skeby <103804946+Skeby@users.noreply.github.com>
2024-07-19 22:44:58 +0100

    fix: sort out lint issues

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -33,20 +33,20 @@ const buttonVariants = cva(
   },
 );
 
-export interface ButtonProps
+export interface ButtonProperties
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+const Button = React.forwardRef<HTMLButtonElement, ButtonProperties>(
+  ({ className, variant, size, asChild = false, ...properties }, reference) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
+        ref={reference}
+        {...properties}
       />
     );
   },
