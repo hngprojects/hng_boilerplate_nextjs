0a4c622a5b553e0b394204eab0d94dd97e23c0bc
Bwesun <maturinnocent@gmail.com>
2024-07-19 16:02:42 +0100

    fix modal covering with div

--- src/components/AddProduct/index.tsx
+++ src/components/AddProduct/index.tsx
@@ -9,55 +9,53 @@ import { DialogOverlay } from "~/components/ui/dialog";
 
 const AddProduct = () => {
     return ( 
-        <div className='dialog-wrap'>
-            <Dialog>
-                <DialogTrigger className="open-add-product">Add Product</DialogTrigger>
-                <DialogOverlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0">
-                    <DialogContent>
-                        <DialogHeader>
-                        <DialogTitle className="modal-title">Add new Product</DialogTitle>
-                        <DialogDescription className="modal-desc">
-                            Create a new product
-                        </DialogDescription>
-                        </DialogHeader>
-                        {/* Product Name */}
-                        <div className="grid w-full max-full items-center gap-1.5">
-                            <Label htmlFor="name">Product name</Label>
-                            <Input name="name" type="text" id="name" placeholder="e.g Jon Doe" />
-                        </div>
-                        {/* Product description */}
-                        <div className="grid w-full gap-1.5">
-                            <Label htmlFor="desc">Product Description</Label>
-                            <Textarea placeholder="add product description" id="desc" />
-                        </div>
-                        {/* Product price */}
-                        <div className="grid w-full max-full items-center gap-1.5">
-                            <Label htmlFor="price">Price</Label>
-                            <Input name="price" type="text" id="price" placeholder="e.g 2000.00" />
-                        </div>
-                        {/* Product Quantity */}
-                        <div className="grid w-full max-full items-center gap-1.5">
-                            <Label htmlFor="quantity">Quantity</Label>
-                            <Input name="quantity" type="text" id="quantity" placeholder="e.g 1000" />
-                        </div>
-                        {/* Upload Images */}
-                        <div className="grid w-full max-w-sm items-center gap-1.5">
-                            <Label htmlFor="images">Upload images</Label>
-                            <Input name="images" id="images" type="file" />
-                        </div>
-                        <div className="grid w-full max-w-sm items-center gap-1.5 img-view-cover">
-                            <div className='img-view'></div>
-                            <div className='img-view'></div>
-                            <div className='img-view'></div>
-                        </div>
-                        <DialogFooter>
-                            {/* Submit Button */}
-                            <Button type="submit" className="add-product-btn">Add Product</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </DialogOverlay>
-            </Dialog>
-        </div>
+        <Dialog>
+            <DialogTrigger className="open-add-product">Add Product</DialogTrigger>
+            <DialogOverlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0">
+                <DialogContent>
+                    <DialogHeader>
+                    <DialogTitle className="modal-title">Add new Product</DialogTitle>
+                    <DialogDescription className="modal-desc">
+                        Create a new product
+                    </DialogDescription>
+                    </DialogHeader>
+                    {/* Product Name */}
+                    <div className="grid w-full max-full items-center gap-1.5">
+                        <Label htmlFor="name">Product name</Label>
+                        <Input name="name" type="text" id="name" placeholder="e.g Jon Doe" />
+                    </div>
+                    {/* Product description */}
+                    <div className="grid w-full gap-1.5">
+                        <Label htmlFor="desc">Product Description</Label>
+                        <Textarea placeholder="add product description" id="desc" />
+                    </div>
+                    {/* Product price */}
+                    <div className="grid w-full max-full items-center gap-1.5">
+                        <Label htmlFor="price">Price</Label>
+                        <Input name="price" type="text" id="price" placeholder="e.g 2000.00" />
+                    </div>
+                    {/* Product Quantity */}
+                    <div className="grid w-full max-full items-center gap-1.5">
+                        <Label htmlFor="quantity">Quantity</Label>
+                        <Input name="quantity" type="text" id="quantity" placeholder="e.g 1000" />
+                    </div>
+                    {/* Upload Images */}
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                        <Label htmlFor="images">Upload images</Label>
+                        <Input name="images" id="images" type="file" />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5 img-view-cover">
+                        <div className='img-view'></div>
+                        <div className='img-view'></div>
+                        <div className='img-view'></div>
+                    </div>
+                    <DialogFooter>
+                        {/* Submit Button */}
+                        <Button type="submit" className="add-product-btn">Add Product</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </DialogOverlay>
+        </Dialog>
      );
 }
  
