2c6bd5a3c3fab5f55c9669dc1c15e0e14dfd0111
Ayodejioladimeji <layobright11@gmail.com>
2024-07-19 22:23:29 +0100

    Implemented the homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
+import "~/components/layouts/homepage/styles/styles.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
