485e51273efe82ca04d7fac0aa6b0f6e01db15ef
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-19 19:01:08 +0100

    refactor: Rearrange classnames

--- src/components/common/Badge/BadgeInterface.ts
+++
@@ -1,9 +0,0 @@
-// Interface for the 'Badge' component
-
-interface BadgeInterface {
-  label: string;
-  icon?: React.ReactNode;
-  variant: "default" | "primary" | "success" | "error";
-}
-
-export default BadgeInterface;
