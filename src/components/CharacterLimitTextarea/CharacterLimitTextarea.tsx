cd9d254f332f6b2280b1b0c88423c4a3cb607716
JoshuaAkpan <joshuaakpan7@gmail.com>
2024-07-19 03:37:32 +0100

    feat: add dynamic styling

--- src/components/CharacterLimitTextarea/CharacterLimitTextarea.tsx
+++ src/components/CharacterLimitTextarea/CharacterLimitTextarea.tsx
@@ -27,10 +27,10 @@ const CharacterLimitTextarea = ({
   };
 
   return (
-    <div className="">
+    <div className="relative">
       <label
         htmlFor={id}
-        className=""
+        className="text-[#0F172A] mb-[3px] font-medium text-sm"
       >
         {label}
       </label>
@@ -40,19 +40,21 @@ const CharacterLimitTextarea = ({
         name={name}
         onChange={handleChange}
         aria-describedby="textarea-info textarea-error"
-        className={``}
+        className={`text-[#0F172A] font-normal text-sm resize-none rounded-md w-full max-w-[826px] focus:outline-none focus:border-[#0F172A] bg-white box-border h-20 py-[8px] px-[12px] border-[1px] border-solid ${
+          error ? "border-[#DC2626]" : ""
+        }`}
       />
       {!error ? (
         <div
           id="textarea-info"
-          className=""
+          className="mt-[6px] font-normal text-sm text-gray-500"
         >
           {charCount}/{maxLength} characters
         </div>
       ) : (
         <div
           id="textarea-error"
-          className=""
+          className="mt-[6px] font-normal text-sm text-[#DC2626]"
         >
           {error}
         </div>
