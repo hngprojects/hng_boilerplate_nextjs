18074291ebdb2606be3ffb16c9852b885bcb2fde
Emmanuel-Xs <emmanuelnwaohiri1@gmail.com>
2024-07-20 20:50:50 +0100

    fix: remove unnecessary comments and fragments

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -19,7 +19,6 @@ export default function PrivacyPolicy() {
       "md:scroll-pt-10",
     );
 
-    // Clean up on unmount
     return () => {
       htmlElement.classList.remove(
         "scroll-smooth",
@@ -30,25 +29,23 @@ export default function PrivacyPolicy() {
   }, []);
 
   return (
-    <>
-      <main className="min-h-svh">
-        {/* hero-sections */}
-        <p>sub page hero section</p>
-        <div className="mx-auto my-10 w-[min(100%-32px_,_996px)] md:my-16">
-          {/* breadcrumbs */}
-          <p>breadcrumbs</p>
-          <section className="mt-14 grid grid-cols-1 gap-20 md:mt-[86px] md:grid-cols-[1fr,306px] md:gap-[73px]">
-            <TableOfContent
-              listOfContent={tableOfContents}
-              className="top-10 max-w-[306px] md:sticky md:col-start-3 md:h-svh"
-            />
-            <PrivacyPolicyContent
-              content={privacyPolicyData}
-              className="max-w-[617px] md:col-span-2 md:col-start-1 md:row-start-1"
-            />
-          </section>
-        </div>
-      </main>
-    </>
+    <main className="min-h-svh">
+      {/* hero-sections */}
+      <p>sub page hero section</p>
+      <div className="mx-auto my-10 w-[min(100%-32px_,_996px)] md:my-16">
+        {/* breadcrumbs */}
+        <p>breadcrumbs</p>
+        <section className="mt-14 grid grid-cols-1 gap-20 md:mt-[86px] md:grid-cols-[1fr,306px] md:gap-[73px]">
+          <TableOfContent
+            listOfContent={tableOfContents}
+            className="top-10 max-w-[306px] md:sticky md:col-start-3 md:h-svh"
+          />
+          <PrivacyPolicyContent
+            content={privacyPolicyData}
+            className="max-w-[617px] md:col-span-2 md:col-start-1 md:row-start-1"
+          />
+        </section>
+      </div>
+    </main>
   );
 }
