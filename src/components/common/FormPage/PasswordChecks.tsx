9a57e010a6bf82ce0e075c7b59243462d5130732
Abd'Quadri Jinad <abdjinadquadri@gmail.com>
2024-07-20 13:52:10 +0100

    Ran eslint

--- src/components/common/FormPage/PasswordChecks.tsx
+++ src/components/common/FormPage/PasswordChecks.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import CheckmarkIcon from "./CheckmarkIcon";
 
-interface PasswordChecksProps {
+interface PasswordChecksProperties {
   checks: {
     uppercase: boolean;
     number: boolean;
@@ -10,7 +10,7 @@ interface PasswordChecksProps {
   };
 }
 
-const PasswordChecks: React.FC<PasswordChecksProps> = ({ checks }) => {
+const PasswordChecks: React.FC<PasswordChecksProperties> = ({ checks }) => {
   return (
     <>
       <div className="flex gap-x-4">
@@ -26,7 +26,7 @@ const PasswordChecks: React.FC<PasswordChecksProps> = ({ checks }) => {
         </div>
         <div className="h-1 w-24 overflow-hidden rounded-sm bg-[#e0e0e0]">
           <div
-            className={`h-full rounded-sm transition-all duration-300 ${checks.length ? "bg-green-600" : "bg-[#e0e0e0]"} ${checks.length || checks.number || checks.uppercase ? "w-full" : "w-0"}`}
+            className={`h-full rounded-sm transition-all duration-300 ${checks.length > 0 ? "bg-green-600" : "bg-[#e0e0e0]"} ${checks.length > 0 || checks.number || checks.uppercase ? "w-full" : "w-0"}`}
           ></div>
         </div>
       </div>
@@ -41,7 +41,7 @@ const PasswordChecks: React.FC<PasswordChecksProps> = ({ checks }) => {
           <span>At least 1 number</span>
         </div>
         <div className="flex items-center gap-x-1 text-[#434343]">
-          <CheckmarkIcon color={checks.length ? "#6DC347" : "#DC2626"} />
+          <CheckmarkIcon color={checks.length > 0 ? "#6DC347" : "#DC2626"} />
           <span>At least 8 characters</span>
         </div>
       </div>
