57a1394cb5d7a1aa6cb2c8d4f2f1eaf211847e38
WebKing <isaac@afrocenchix.com>
2024-07-20 15:35:35 +0100

    Refactor: test code in the page.tsx removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import HowItWorks from "~/components/layouts/homepage/HowItWorks";
 import PerfectFit from "~/components/layouts/homepage/PerfectFit";
 import Testimonials from "~/components/layouts/homepage/Testimonials";
 import UserSection from "~/components/layouts/homepage/UserSection";
-import TopProductLists from "../components/topproductlists/TopProductLists";
 
 export default function Home() {
   return (
     <>
-      <TopProductLists />
       <Hero />
       <UserSection />
       <HowItWorks />
