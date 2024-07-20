140fbd63b4517afbfe0583fae079a70358492c55
Abd'Quadri Jinad <abdjinadquadri@gmail.com>
2024-07-20 18:52:15 +0100

    Change layout and pnpm-lock.yaml due to pnpm install

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -7,10 +7,9 @@ export default function SettingsLayout({
 }) {
   return (
     <>
-      <header>Navbar</header>
       <SettingsSidebar />
       <section className="relative w-full overflow-x-hidden md:pl-[126px] min-[1140px]:pl-[290px]">
-        <div className="relative flex h-full w-full flex-col pt-12 md:pt-0">
+        <div className="relative flex h-full w-full flex-col md:pt-0">
           {children}
         </div>
       </section>
