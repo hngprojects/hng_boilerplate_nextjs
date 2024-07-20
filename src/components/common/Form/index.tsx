63ff1ef3507fcae94755758ba132fb89ec815c92
omotoso eniola <omotosoeniola2@gmail.com>
2024-07-20 15:07:09 +0100

    "Add product table list"
    your changes. Lines starting

---
+++ src/components/common/Form/index.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
+import { formSchema, imageSchema } from "./schema";
+
+function handleSubmit() {
+  console.log();
+}
+function TableForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: "",
+    },
+  });
+  const formImage = useForm<z.infer<typeof imageSchema>>({
+    resolver: zodResolver(imageSchema),
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="flex flex-col gap-[15px]"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => {
+            return (
+              <FormItem className="space-y-0">
+                <FormLabel className="text-[14px] font-medium">
+                  Product name
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    id="name"
+                    placeholder="e.g Joh Doe"
+                    {...field}
+                    className="mt-[6px]"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => {
+            return (
+              <FormItem className="space-y-0">
+                <FormLabel>Product Decription</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="add product description"
+                    id="description"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => {
+            return (
+              <FormItem className="space-y-0">
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    id="price"
+                    placeholder="e.g 2000.00"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={form.control}
+          name="quantity"
+          render={({ field }) => {
+            return (
+              <FormItem className="space-y-0">
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    id="quantity"
+                    placeholder="e.g 1000"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={formImage.control}
+          name="image"
+          render={({ field }) => {
+            return (
+              <FormItem className="space-y-1">
+                <FormLabel className="">Upload Images</FormLabel>
+                <FormControl>
+                  <Input type="file" id="image" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            className="bg-[#E77F1E] text-sm text-white hover:bg-[#E77F1E]"
+          >
+            Add Product
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
+
+export default TableForm;
---
+++ src/components/common/ProductFilter/index.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+import { Button } from "~/components/ui/button";
+
+function ProductFilter() {
+  return (
+    <div className="mb-10 flex w-full max-w-sm items-center justify-between md:max-w-md lg:max-w-screen-2xl">
+      <div className="">
+        <h2 className="text-[20px] font-semibold text-black lg:text-[28px]">
+          Products
+        </h2>
+        <p className="min-w-9 text-xs font-normal lg:max-w-[328px]">
+          Manage your products and view their sales performance.
+        </p>
+      </div>
+      <div className="flex gap-[10px]">
+        <Button className="flex items-center gap-[10px] rounded border-[.5px] border-[#CBD5E1] bg-[#FFF] text-base font-normal text-[#525252]">
+          <Image
+            src="/images/filter.svg"
+            width={20}
+            height={20}
+            alt=""
+            className="h-4 w-4 lg:h-[20px] lg:w-[20px]"
+          />
+          <span>Filter</span>
+        </Button>
+        <Button className="flex items-center gap-2 rounded bg-[#F97316] text-white hover:bg-[#F97316]">
+          <Image src="/images/add.svg" width={16} height={16} alt="" />
+          <span>Add Product</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductFilter;
