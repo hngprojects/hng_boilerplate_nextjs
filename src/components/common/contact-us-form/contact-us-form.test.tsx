e5b9d26f4e0d21b9e796eb623e5c29585654a4e0
Glad Chukwu <nellg.gc@gmail.com>
2024-07-20 07:33:51 -0700

    fix: Custom button

--- src/components/common/contact-us-form/contact-us-form.test.tsx
+++ src/components/common/contact-us-form/contact-us-form.test.tsx
@@ -1,8 +1,9 @@
-import ContactForm from "."; // Adjust the import path as needed
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 
-describe("ContactForm Component", () => {
+import ContactForm from "~/components/common/contact-us-form"; // Adjust the import path as needed
+
+describe("contactForm Component", () => {
   const mockFetch = vi.fn();
 
   beforeAll(() => {
@@ -21,13 +22,22 @@ describe("ContactForm Component", () => {
     const phoneInput = screen.getByPlaceholderText("Enter phone number");
     const messageInput = screen.getByPlaceholderText("Message...");
     const submitButton = screen.getByText("Send");
-    return { container, nameInput, emailInput, phoneInput, messageInput, submitButton };
+    return {
+      container,
+      nameInput,
+      emailInput,
+      phoneInput,
+      messageInput,
+      submitButton,
+    };
   };
 
-  it("should validate the form inputs", async () => {
+  it("should validate all required form fields", async () => {
     const { submitButton } = setup();
     fireEvent.click(submitButton);
-    expect(await screen.findAllByText(/is required/)).toHaveLength(4);
+
+    // Ensure that the error messages for all required fields are present
+    await expect(screen.findAllByText(/is required/)).resolves.toHaveLength(2);
   });
 
   it("should validate email format", async () => {
@@ -42,11 +52,13 @@ describe("ContactForm Component", () => {
     const { phoneInput, submitButton } = setup();
     fireEvent.change(phoneInput, { target: { value: "123" } });
     fireEvent.click(submitButton);
-    expect(await screen.findByText("Phone number is invalid")).toBeInTheDocument();
+    const errorMessage = await screen.findByText("Phone number is invalid");
+    expect(errorMessage).toBeInTheDocument();
   });
 
   it("should submit the form successfully", async () => {
-    const { nameInput, emailInput, phoneInput, messageInput, submitButton } = setup();
+    const { nameInput, emailInput, phoneInput, messageInput, submitButton } =
+      setup();
     mockFetch.mockResolvedValueOnce({
       ok: true,
       json: async () => ({ message: "Form submitted successfully!" }),
@@ -60,11 +72,14 @@ describe("ContactForm Component", () => {
     fireEvent.click(submitButton);
 
     await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
-    expect(screen.getByText("Form submitted successfully!")).toBeInTheDocument();
+    expect(
+      screen.getByText("Form submitted successfully!"),
+    ).toBeInTheDocument();
   });
 
   it("should handle form submission error", async () => {
-    const { nameInput, emailInput, phoneInput, messageInput, submitButton } = setup();
+    const { nameInput, emailInput, phoneInput, messageInput, submitButton } =
+      setup();
     mockFetch.mockResolvedValueOnce({
       ok: false,
       json: async () => ({ message: "Failed to submit the form." }),
@@ -82,7 +97,8 @@ describe("ContactForm Component", () => {
   });
 
   it("should reset status and message after 3 seconds", async () => {
-    const { nameInput, emailInput, phoneInput, messageInput, submitButton } = setup();
+    const { nameInput, emailInput, phoneInput, messageInput, submitButton } =
+      setup();
     mockFetch.mockResolvedValueOnce({
       ok: true,
       json: async () => ({ message: "Form submitted successfully!" }),
@@ -96,23 +112,27 @@ describe("ContactForm Component", () => {
     fireEvent.click(submitButton);
 
     await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
-    expect(screen.getByText("Form submitted successfully!")).toBeInTheDocument();
+    expect(
+      screen.getByText("Form submitted successfully!"),
+    ).toBeInTheDocument();
 
     // Use fake timers for this specific test case
     vi.useFakeTimers();
     vi.advanceTimersByTime(3000);
-    // Clear the fake timers
     vi.useRealTimers();
   });
 
   it("should be responsive", async () => {
     const { container } = setup();
+
+    // Check mobile responsiveness
     window.innerWidth = 320; // Mobile width
-    window.dispatchEvent(new Event('resize'));
-    expect(container.firstChild).toHaveClass('w-full');
-    
+    window.dispatchEvent(new Event("resize"));
+    expect(container.firstChild).toHaveClass("w-full");
+
+    // Check desktop responsiveness
     window.innerWidth = 1024; // Desktop width
-    window.dispatchEvent(new Event('resize'));
-    expect(container.firstChild).toHaveClass('max-w-[80%]');
+    window.dispatchEvent(new Event("resize"));
+    expect(container.firstChild).toHaveClass("max-w-[80%]");
   });
 });
--- src/components/common/contact-us-form/index.tsx
+++ src/components/common/contact-us-form/index.tsx
@@ -1,27 +1,24 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { z, ZodError } from "zod";
 
-import InputField from "./inputfield";
+import InputField from "~/components/common/contact-us-form/inputfield";
+import CustomButton from "../Button/button";
+import Image from "next/image";
 
-interface FormData {
-  [key: string]: string;
-}
+const schema = z.object({
+  name: z.string().min(5, "Name is required"),
+  email: z.string().email("Email is invalid"),
+  phone: z
+    .string()
+    .min(8, "Phone number is required")
+    .regex(/^\+?\d{10,15}$/, "Phone number is invalid"),
+  message: z.string().min(1, "Message is required"),
+});
 
-const validationRules: { [key: string]: (value: string) => string | null } = {
-  name: (value) => (!value ? "Name is required" : null),
-  email: (value) => {
-    if (!value) return "Email is required";
-    if (!/\S+@\S+\.\S+/.test(value)) return "Email is invalid";
-    return null;
-  },
-  phone: (value) => {
-    if (!value) return "Phone number is required"; // Phone number is optional
-    if (!/^\+?[0-9]{10,15}$/.test(value)) return "Phone number is invalid";
-    return null;
-  },
-  message: (value) => (!value ? "Message is required" : null),
-};
+// Define the FormData interface
+type FormData = z.infer<typeof schema>;
 
 const initialFormData: FormData = {
   name: "",
@@ -33,56 +30,61 @@ const initialFormData: FormData = {
 const ContactForm: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({ ...initialFormData });
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
-  const [status, setStatus] = useState<boolean | null>(null);
-  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
-  const [message, setMessage] = useState<string | null>(null);
+  const [status, setStatus] = useState<boolean | undefined>();
+  const [timer, setTimer] = useState<NodeJS.Timeout | undefined>();
+  const [message, setMessage] = useState<string | undefined>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    if (status !== null) {
-      // clear existing timer
-      if (timer != null) {
+    if (status !== undefined) {
+      if (timer != undefined) {
         clearTimeout(timer);
       }
       const newTimer = setTimeout(() => {
-        setStatus(null);
+        setStatus(undefined);
       }, 3000);
       setTimer(newTimer);
     }
 
     return () => {
-      // clear timer on unmount
-      if (timer != null) {
+      if (timer != undefined) {
         clearTimeout(timer);
       }
     };
-  }, [status]);
+  }, [status, timer]);
 
   const validate = () => {
-    const errors: { [key: string]: string } = {};
-    Object.keys(formData).forEach((key) => {
-      const error = validationRules[key]?.(formData[key]);
-      if (error) {
-        errors[key] = error;
+    try {
+      schema.parse(formData);
+      return {};
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages: { [key: string]: string } = {};
+        for (const { path, message } of error.errors) {
+          errorMessages[path[0]] = message;
+        }
+        return errorMessages;
       }
-    });
-    return errors;
+      return {};
+    }
   };
 
   const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
-    const { name, value } = e.target;
+    const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     const validationErrors = validate();
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
       return;
     }
     try {
+      setLoading(true);
       const response = await fetch(
         "https://test.gracefilledcollege.com/public/api/v1/contact",
         {
@@ -94,22 +96,20 @@ const ContactForm: React.FC = () => {
         },
       );
 
-      // Parse the response body as JSON
       const responseData = await response.json();
 
-      // Check if response is not OK
       if (!response.ok) {
-        // Use the error message from the response, if available
         throw new Error(responseData.message || "Failed to submit the form.");
       }
 
-      // Set success status and message
       setStatus(true);
-      setMessage(responseData.message || "Form submitted successfully!");
+      setMessage(responseData?.message || "Form submitted successfully!");
       setFormData({ ...initialFormData });
-      setErrors({});  // Clear errors
+      setErrors({});
+      setLoading(false);
     } catch (error) {
       setStatus(false);
+      setLoading(false);
       setMessage(
         (error as Error).message ||
           "Failed to submit the form. Please try again.",
@@ -137,7 +137,7 @@ const ContactForm: React.FC = () => {
       name: "phone",
       type: "tel",
       placeholder: "Enter phone number",
-      required: false,
+      required: true,
     },
   ];
 
@@ -146,13 +146,12 @@ const ContactForm: React.FC = () => {
       <div className="mx-auto w-full max-w-[80%] p-8">
         <form
           onSubmit={handleSubmit}
-          className="mb-4 w-full max-w-[80%] rounded bg-[#FAFAFA] px-8 pb-8 pt-6"
+          className="mb-4 w-full max-w-[80%] rounded bg-subtle px-8 pb-8 pt-6"
           role="form"
         >
           {inputFields.map((field) => (
             <div key={field.name} className="mb-4">
               <InputField
-                key={field.name}
                 value={formData[field.name as keyof FormData]}
                 type={field.type}
                 onChange={handleChange}
@@ -162,17 +161,14 @@ const ContactForm: React.FC = () => {
                 label={field.label}
               />
               {errors[field.name] && (
-                <p className="text-xs italic text-red-500">
+                <p className="text-xs italic text-destructive">
                   {errors[field.name]}
                 </p>
               )}
             </div>
           ))}
           <div className="mb-4">
-            <label
-              htmlFor="message"
-              className="mb-2 block text-sm text-gray-700"
-            >
+            <label htmlFor="message" className="mb-2 block text-sm">
               Message
             </label>
             <input
@@ -181,28 +177,27 @@ const ContactForm: React.FC = () => {
               placeholder="Message..."
               value={formData.message}
               onChange={handleChange}
-              className="w-full appearance-none rounded border-2 bg-[#FAFAFA] px-3 py-2 pb-[112px] leading-tight text-gray-700"
+              className="w-full appearance-none rounded border-2 bg-subtle px-3 py-2 pb-[112px] leading-tight"
             />
             {errors.message && (
-              <p className="text-xs italic text-red-500">{errors.message}</p>
+              <p className="text-xs italic text-destructive">
+                {errors.message}
+              </p>
             )}
           </div>
-          <button
-            type="submit"
-            className="focus:shadow-outline flex w-full items-center justify-center gap-2 rounded bg-[#F97316] px-4 py-3 text-[0.65rem] font-normal text-white focus:outline-none"
+          <CustomButton
+            variant="primary"
+            size="lg"
+            isLoading={loading}
+            className="w-full px-4 py-3"
           >
-            <img src="/mail.svg" alt="mail" />
+           <Image src={"/mail.svg"} width={20} height={10} alt="send icon"/>
             Send
-          </button>
-          {status && (
-            <p className={`text-xs italic text-green-500`}>
-              {message}
-            </p>
-          )}
-          {status == false && (
-            <p className={`text-xs italic text-red-500`}>
-              {message}
-            </p>
+          </CustomButton>
+
+          {status && <p className={`text-xs italic text-default`}>{message}</p>}
+          {status === false && (
+            <p className={`text-xs italic text-destructive`}>{message}</p>
           )}
         </form>
       </div>
