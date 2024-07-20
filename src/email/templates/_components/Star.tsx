b3a7fee3385195af286415c3f98b1f2495833a2c
Russell Oje <russelloje@gmail.com>
2024-07-20 19:10:24 +0100

    Implemented Email Template for Welcome with Image

---
+++ src/email/templates/_components/Star.tsx
@@ -0,0 +1,17 @@
+export default function Star() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+    >
+      <path
+        d="M12 0.5L15.2414 9.25862L24 12.5L15.2414 15.7414L12 24.5L8.75862 15.7414L0 12.5L8.75862 9.25862L12 0.5Z"
+        fill="#F97316"
+        fillOpacity="0.4"
+      />
+    </svg>
+  );
+}
