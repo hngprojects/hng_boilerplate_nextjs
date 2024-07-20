a92e1ad21d77b531c2acca81cc4764bf3fd38a5d
Aminah Rashid <aminahrashid.bsaif20@iba-suk.edu.pk>
2024-07-20 14:37:46 +0500

    feat: create a product details card component and perform testing

---
+++ src/components/common/ProductDetailsCard/ProductDetailsCard.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+
+import CustomButton from "../Button/button";
+
+interface ProductDetailsProperties {
+  productName?: string;
+  productImageSrc?: string;
+  productID?: string;
+  category?: string;
+  dateAdded?: string;
+  stock?: string;
+  price?: string;
+  description?: string;
+  onClose: () => void;
+  onEdit: (productID: string) => void;
+  onDelete: (productID: string) => void;
+}
+
+const ProductDetailsCard: React.FC<ProductDetailsProperties> = ({
+  productName = "N/A",
+  productImageSrc: productImageSource = "/default-image.jpg",
+  productID = "N/A",
+  category = "N/A",
+  dateAdded = "N/A",
+  stock = "N/A",
+  price = "N/A",
+  description = "No description available",
+  onClose,
+  onEdit,
+  onDelete,
+}) => {
+  const handleEdit = () => {
+    if (productID === "N/A") {
+      console.error("Product ID is missing. Cannot edit.");
+    } else {
+      onEdit(productID);
+    }
+  };
+
+  const handleDelete = () => {
+    if (productID === "N/A") {
+      console.error("Product ID is missing. Cannot delete.");
+    } else {
+      onDelete(productID);
+    }
+  };
+
+  if (
+    !productName ||
+    !productImageSource ||
+    !productID ||
+    !category ||
+    !dateAdded ||
+    !stock ||
+    !price ||
+    !description
+  ) {
+    return (
+      <div className="inline-flex max-w-[403px] flex-col justify-center rounded-md border border-solid border-input py-6 pl-[1.4375rem] pr-[1.5rem] text-sm shadow-[0px_1px_18px_rgba(10,_57,_176,_0.12)] shadow-sm transition-all duration-500">
+        <div className="text-error text-center">
+          Some product details are missing. Please provide complete product
+          information.
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="inline-flex max-w-[403px] flex-col justify-center rounded-md border border-solid border-border py-6 pl-[1.4375rem] pr-[1.5rem] text-sm shadow-[0px_1px_18px_rgba(10,_57,_176,_0.12)] transition-all duration-500">
+      <header className="text-neutral-dark-2 flex justify-between gap-5 px-px text-xl font-semibold">
+        <div>{productName}</div>
+        <div onClick={onClose}>
+          <Image
+            src="/icons/close.svg"
+            alt="Close Icon"
+            width={24}
+            height={24}
+            className="aspect-square h-6 w-6 shrink-0 cursor-pointer"
+          />
+        </div>
+      </header>
+      <section className="flex flex-col gap-4 self-stretch">
+        <Image
+          src={productImageSource}
+          alt={`Product ${productName} Image`}
+          width={403}
+          height={174}
+          className="mt-6 aspect-[2.33] w-full self-stretch rounded-[0.375rem] object-cover"
+        />
+        <div className="relative box-border h-px border-t-[1px] border-solid border-border" />
+        <div className="flex items-center justify-between gap-5 self-stretch whitespace-nowrap">
+          <div className="text-neutral-dark-1 text-sm">Product ID</div>
+          <div className="text-neutral-dark-2 text-right text-sm">
+            {productID}
+          </div>
+        </div>
+        <div className="flex items-center justify-between gap-5 self-stretch whitespace-nowrap">
+          <div className="text-neutral-dark-1 text-sm">Category</div>
+          <div className="text-neutral-dark-2 text-right text-sm">
+            {category}
+          </div>
+        </div>
+        <div className="flex items-center justify-between gap-5 self-stretch whitespace-nowrap">
+          <div className="text-neutral-dark-1 text-sm">Date added</div>
+          <div className="text-neutral-dark-2 text-right text-sm">
+            {dateAdded}
+          </div>
+        </div>
+        <div className="flex items-center justify-between gap-5 self-stretch whitespace-nowrap">
+          <div className="text-neutral-dark-1 text-sm">Stock</div>
+          <div className="text-neutral-dark-2 text-right text-sm">{stock}</div>
+        </div>
+        <div className="flex justify-between gap-5 whitespace-nowrap">
+          <div className="text-neutral-dark-1 text-sm">Price</div>
+          <div className="text-neutral-dark-2 text-right text-sm">{price}</div>
+        </div>
+        <div className="relative box-border h-px border-t-[1px] border-solid border-border" />
+        <div className="flex flex-col justify-center gap-[0.625rem] self-stretch rounded-md bg-neutral-50 p-4">
+          <div className="text-neutral-dark-2 text-sm">Description</div>
+          <div className="text-neutral-dark-1 mt-2.5 line-clamp-3 overflow-hidden text-ellipsis text-sm">
+            {description}
+          </div>
+        </div>
+        <div className="relative box-border h-px border-t-[1px] border-solid border-border" />
+      </section>
+      <div className="mt-6 flex gap-2 self-end whitespace-nowrap text-sm font-medium leading-6">
+        <div
+          onClick={handleDelete}
+          className="text-error items-center justify-center rounded-md border border-solid border-border bg-white"
+        >
+          <CustomButton variant="ghost">Delete</CustomButton>
+        </div>
+        <div
+          onClick={handleEdit}
+          className="text-neutral-dark-2 items-center justify-center rounded-md border border-solid border-slate-200 bg-white shadow-none"
+        >
+          <CustomButton variant="ghost">Edit</CustomButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetailsCard;
