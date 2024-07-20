4a77f2ab364da0e07d53d1a8d954666cf1b8dcaa
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-07-20 10:37:43 +0100

    fix: Removed the font-type declaration in the compoonent

--- src/components/external_static_page/SubPageHero.tsx
+++ src/components/external_static_page/SubPageHero.tsx
@@ -8,10 +8,10 @@ const SubPageHero = ({
   description = "Get Your Free Boilerplate Samples!",
 }: SubPageHeroProperty) => {
   return (
-    <div className="font-h5-medium md:font-h3-regular relative box-border flex h-[13.438rem] w-full shrink-0 flex-col items-center justify-center overflow-hidden bg-black bg-opacity-70 px-[0rem] py-[4.687rem] text-center text-[2rem] text-white md:h-[20.25rem] md:text-[3.75rem]">
+    <div className="relative box-border flex h-[13.438rem] w-full shrink-0 flex-col items-center justify-center overflow-hidden bg-black bg-opacity-70 px-[0rem] py-[4.687rem] text-center text-[2rem] text-white md:h-[20.25rem] md:text-[3.75rem]">
       <div className="flex flex-col items-center justify-start gap-[0.25rem] self-stretch">
         <b className="relative self-stretch">{heading}</b>
-        <div className="text-stroke-colors-stroke relative self-stretch text-[1.25rem] font-medium md:text-[1.75rem]">
+        <div className="relative self-stretch text-[1.25rem] md:text-[1.75rem]">
           {description}
         </div>
       </div>
