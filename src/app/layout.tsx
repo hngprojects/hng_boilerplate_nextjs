d419842963d1de64ce697840f84065c95e43ca7c
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-07-20 11:03:13 +0100

    chore: changed requested

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { Toaster } from "~/components/ui/toaster";
+
 import "./globals.css";
 import "~/components/layouts/homepage/styles/styles.css";
 
@@ -20,9 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex h-screen bg-gray-100`}>
-        <Sidebar />
-        <main className="flex-1 overflow-auto"> {children}</main>
+      <body className={inter.className}>
+        {children}
+        <Toaster />
       </body>
     </html>
   );
