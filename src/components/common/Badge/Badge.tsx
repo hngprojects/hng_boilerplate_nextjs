c8b4ecea1997d40e818fc9fce5ae5fbef6d96f0c
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-20 20:58:00 +0100

    docs: Update the style guide with the badge component

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -5,7 +5,11 @@
  * - Icon?: React.ReactNode
  *
  * The commponent can be rendered as follows:
+ * - Import the component and its props ineterface
+ *
+ * ```
  * import Badge, { BadgeInterface } from "~/components/common/Badge/Badge";
+ * ```
  * - Option1: Define the props object, render the component, and spread the object as props to it
  * - Option2: Render the component, and pass in the required props individually
  */
@@ -24,15 +28,15 @@ const Badge = ({ label, icon, variant }: BadgeInterface) => {
     error: "text-[hsl(var(--error))]",
   };
   const badgeBackgroundVariants = {
-    default: "bg-[hsla(var(--default-badge-bg))]",
-    primary: "bg-[hsla(var(--primary-badge-bg))]",
-    success: "bg-[hsla(var(--success-badge-bg))]",
-    error: "bg-[hsla(var(--error-badge-bg))]",
+    default: "bg-[hsl(222,47%,11%)]",
+    primary: "bg-[hsl(25,95%,53%)]",
+    success: "bg-[hsl(102,51%,52%)]",
+    error: "bg-[hsl(0,72%,51%)]",
   };
 
   return (
     <div
-      className={`${badgeBackgroundVariants[variant]} flex max-w-max rounded-[16px]`}
+      className={`${badgeBackgroundVariants[variant]} flex max-w-max rounded-[16px] bg-opacity-10`}
     >
       <div
         className={`${badgeTextVariants[variant]} flex gap-[6px] py-[2px] pl-[6px] pr-2 text-[0.75rem] font-[500] capitalize leading-[18px] md:py-[2px] md:pl-[8px] md:pr-[10px] md:text-[0.875rem] lg:py-1 lg:pl-[10px] lg:pr-3 lg:leading-[20px]`}
