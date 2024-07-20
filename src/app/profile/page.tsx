9ad1900bb28a687c03ddab112faab0b5357eca49
Modupe Akanni <modupe775@gmail.com>
2024-07-20 03:44:38 +0100

    feat: create profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+import Profile from "~/components/layouts/Profile/index";
+
+const page = () => {
+  return (
+    <div className="max-w-screen-2xl 2xl:mx-auto">
+      {/* Nav to go here */}
+      {/* Side Nav to go here */}
+      <Profile />
+      {/* Footer to go here */}
+    </div>
+  );
+};
+
+export default page;
