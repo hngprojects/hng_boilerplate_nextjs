62b225dcd74eefa30fc36dce7710ca401a92e03a
deji2ghost <dejiwilliams9@gmail.com>
2024-07-20 18:21:04 +0200

    feat: Api integration for the latest articles page

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,12 +1,26 @@
-import { render } from "../test/utils";
+import { render,screen,fireEvent } from "../test/utils";
 import Page from "./page";
 
 describe("page tests", () => {
   it("should render correctly", () => {
-    expect.assertions(1);
-
+  
+    // console.log('Hello world')
     render(<Page />);
+    // required-latest-articles
+    expect(screen.getByText('Latest Articles')).toBeInTheDocument()
+    const cardElements = screen.getAllByTestId('card-list');
+    expect(cardElements).toHaveLength(5);
+    const button = screen.getByTestId('buttontestid');
+    fireEvent.click(button)
 
-    expect(true).toBeTruthy();
+      setTimeout(()=>{
+        const cardElementsSecondTime = screen.getAllByTestId('card-list');
+
+
+        expect(cardElementsSecondTime).toHaveLength(6);
+    
+      },2500)
+
+
   });
 });
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -30,25 +30,27 @@ export const Body = () => {
     }
   return (
     <div className='flex flex-col gap-[24px]'>
-        <h1 className='text-[#525252] text-[28px] font-bold md:text-center'>Latest Articles</h1>
+        <h1 className='text-[#525252] text-[28px] font-bold md:text-center'
+        data-testid="required-latest-articles"
+        >Latest Articles</h1>
         <div className='flex flex-col items-start gap-[24px] mb-3 md:w-[70%] md:mx-auto'>
             {
                 mainBlog.slice(0, visible).map(blog=> {
                     return(
-                        <div key={blog.id} className='flex flex-col md:flex-row md:gap-[24px] md:p-[24px] justify-center items-start border-b border-[#525252]'>
+                        <div key={blog.id} className='flex flex-col md:flex-row md:gap-[24px] md:p-[24px] justify-center items-start border-b border-[#525252]' data-testid="card-list">
                             <div className=''>
                                 <span className='px-[12px] py-[4px] text-[12px] font-bold text-center left-2 bg-[#F97316] rounded-[16px] text-[#525252]'>{blog.occupation}</span>
                                 <h1 className='text-[20px] font-bold leading-normal'>{blog.header}</h1>
                                 <p className='text-[14px] font-normal leading-normal'>{blog.description}</p>
                                 <div className='flex items-center gap-[6px]'>
-                                    <Image width={24} src={blog.accountImgMobile} alt='image'/>
+                                    <img width={24}  src={blog.accountImgMobile.src} alt='image'/>
                                     <p className='text-[14px] font-medium leading-normal'>{blog.name}</p>
                                     <p className='text-[14px] font-normal leading-normal'>{blog.readTime}</p>
                                     <p className='text-[14px] font-normal leading-normal'>{blog.date}</p>
                                 </div>
                             </div>
                             <div className='relative'>
-                                <Image src={blog.image} alt='picture'></Image>
+                                <img src={blog.image.src} alt='picture'></img>
                             </div>
                         </div>
                     )
@@ -56,7 +58,9 @@ export const Body = () => {
             }
         </div>
 
-        <button onClick={showMoreItem} className="w-full md:w-[20%] lg:w-[11%] md:mx-auto bg-[#F97316] rounded-md text-center text-[14px] font-medium text-primary-foreground py-[9px] leading-[25px]">{loading ? 'LOADING' : "Load More"}</button>
+        <button
+        data-testid='buttontestid'
+        onClick={showMoreItem} className="w-full md:w-[20%] lg:w-[11%] md:mx-auto bg-[#F97316] rounded-md text-center text-[14px] font-medium text-primary-foreground py-[9px] leading-[25px]">{loading ? 'LOADING' : "Load More"}</button>
     </div>
   )
 }
