395b1f19806289f5baaa07f5a87dac44ebcfca9a
Emmanuel-Xs <emmanuelnwaohiri1@gmail.com>
2024-07-20 18:20:12 +0100

    feat: add client-side smooth scrolling workaround to PrivacyPolicy page
    - Implement `useEffect` hook to dynamically add and remove CSS classes for smooth scrolling on the <html> element.
    - This serves as a workaround to avoid modifying the main layout.tsx file.

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect } from "react";
+
 import TableOfContent from "~/components/layouts/LegalTerms/TableOfContent";
 import PrivacyPolicyContent from "~/components/layouts/PrivacyPolicy/PrivacyPolicyContent";
 import privacyPolicyData, {
@@ -7,24 +11,44 @@ import privacyPolicyData, {
 export default function PrivacyPolicy() {
   const tableOfContents = getTableOfContents(privacyPolicyData);
 
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+    htmlElement.classList.add(
+      "scroll-smooth",
+      "scroll-pt-6",
+      "md:scroll-pt-10",
+    );
+
+    // Clean up on unmount
+    return () => {
+      htmlElement.classList.remove(
+        "scroll-smooth",
+        "scroll-pt-6",
+        "md:scroll-pt-10",
+      );
+    };
+  }, []);
+
   return (
-    <main className="min-h-svh">
-      {/* hero-sections */}
-      <p>sub page hero section</p>
-      <div className="mx-auto my-10 w-[min(100%-32px_,_996px)] md:my-16">
-        {/* breadcrumbs */}
-        <p>breadcrumbs</p>
-        <section className="mt-14 grid grid-cols-1 gap-20 md:mt-[86px] md:grid-cols-[1fr,306px] md:gap-[73px]">
-          <TableOfContent
-            listOfContent={tableOfContents}
-            className="top-10 max-w-[306px] md:sticky md:col-start-3 md:h-svh"
-          />
-          <PrivacyPolicyContent
-            content={privacyPolicyData}
-            className="max-w-[617px] md:col-span-2 md:col-start-1 md:row-start-1"
-          />
-        </section>
-      </div>
-    </main>
+    <>
+      <main className="min-h-svh">
+        {/* hero-sections */}
+        <p>sub page hero section</p>
+        <div className="mx-auto my-10 w-[min(100%-32px_,_996px)] md:my-16">
+          {/* breadcrumbs */}
+          <p>breadcrumbs</p>
+          <section className="mt-14 grid grid-cols-1 gap-20 md:mt-[86px] md:grid-cols-[1fr,306px] md:gap-[73px]">
+            <TableOfContent
+              listOfContent={tableOfContents}
+              className="top-10 max-w-[306px] md:sticky md:col-start-3 md:h-svh"
+            />
+            <PrivacyPolicyContent
+              content={privacyPolicyData}
+              className="max-w-[617px] md:col-span-2 md:col-start-1 md:row-start-1"
+            />
+          </section>
+        </div>
+      </main>
+    </>
   );
 }
