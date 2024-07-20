ad2c9600487fe31287d8695e331c8d8ba5890cc1
itsjustonyii <onyinyechiokeke733@gmail.com>
2024-07-20 07:25:38 +0100

    feat: move my modal to components/modals

--- src/components/modals/DeleteModal/index.tsx
+++ src/components/modals/DeleteModal/index.tsx
@@ -1,7 +1,16 @@
-'use client';
-import React from 'react';
+"use client";
+
+import React from "react";
+
 import CustomButton from "~/components/common/Button/button";
-import { Dialog, DialogOverlay, DialogContent, DialogHeader, DialogTitle, DialogDescription }  from "../../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogOverlay,
+  DialogTitle,
+} from "../../ui/dialog";
 
 interface DeleteModalProps {
   closeDeleteModal: () => void;
@@ -9,28 +18,48 @@ interface DeleteModalProps {
 
 const DeleteModal: React.FC<DeleteModalProps> = ({ closeDeleteModal }) => {
   return (
-    <Dialog open={true} onOpenChange={closeDeleteModal}> 
-    <DialogOverlay data-testid="modal-container" className="absolute bg-subtle bg-opacity-[25%] w-full top-0 bottom-0 h-[1024px] z-[999] cursor-pointer" onClick={closeDeleteModal}>
-      <DialogContent data-testid="modal-content"
-        className="absolute max-w-[1440px] lg:left-[40%] lg:top-[425px] top-[300px] lg:w-[512px] w-[95%] left-[50%] lg:right-0 bg-background p-[24px] rounded-[6px]">
-        <DialogHeader>
-          <DialogTitle data-testid="modal-heading" className="text-[18px] font-[600] leading-[28px] text-foreground">Delete Member</DialogTitle>
-        </DialogHeader>
-        <DialogDescription data-testid="modal-message" className="text-[14px] font-[400] leading-[16.69px] text-foreground">
-          Are you sure you want to delete Chad Bosewick? All of your data will be permanently removed. This action
-          cannot be undone.
-        </DialogDescription>
-        <div data-testid="modal-buttons" className="flex justify-end gap-[8px]">
-          <div  onClick={closeDeleteModal} >
-            <CustomButton variant="outline" data-testid="cancel-button">Cancel</CustomButton>
-          </div>
+    <Dialog open={true} onOpenChange={closeDeleteModal}>
+      <DialogOverlay
+        data-testid="modal-container"
+        className="absolute bottom-0 top-0 z-[999] h-[1024px] w-full cursor-pointer bg-subtle bg-opacity-[25%]"
+        onClick={closeDeleteModal}
+      >
+        <DialogContent
+          data-testid="modal-content"
+          className="absolute left-[50%] top-[300px] w-[95%] max-w-[1440px] rounded-[6px] bg-background p-[24px] lg:left-[40%] lg:right-0 lg:top-[425px] lg:w-[512px]"
+        >
+          <DialogHeader>
+            <DialogTitle
+              data-testid="modal-heading"
+              className="text-[18px] font-[600] leading-[28px] text-foreground"
+            >
+              Delete Member
+            </DialogTitle>
+          </DialogHeader>
+          <DialogDescription
+            data-testid="modal-message"
+            className="text-[14px] font-[400] leading-[16.69px] text-foreground"
+          >
+            Are you sure you want to delete Chad Bosewick? All of your data will
+            be permanently removed. This action cannot be undone.
+          </DialogDescription>
+          <div
+            data-testid="modal-buttons"
+            className="flex justify-end gap-[8px]"
+          >
+            <div onClick={closeDeleteModal}>
+              <CustomButton variant="outline" data-testid="cancel-button">
+                Cancel
+              </CustomButton>
+            </div>
 
-          <div  onClick={closeDeleteModal} >
-            <CustomButton variant="destructive" data-testid="cancel-button">Delete</CustomButton>
+            <div onClick={closeDeleteModal}>
+              <CustomButton variant="destructive" data-testid="cancel-button">
+                Delete
+              </CustomButton>
+            </div>
           </div>
-          
-          </div>
-      </DialogContent>
+        </DialogContent>
       </DialogOverlay>
     </Dialog>
   );
