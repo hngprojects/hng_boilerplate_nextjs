e3dbaeec260aa72abf54a6aca85e372131bf0bee
Mahmud1087 <abdulazeezm578@gmail.com>
2024-07-20 15:33:21 +0100

    feat(HNG-68): create reusable empty list component that passes eslint

--- src/components/common/Empty List/EmptyList.tsx
+++ src/components/common/Empty List/EmptyList.tsx
@@ -17,17 +17,10 @@ const EmptyList = ({ image, mainText, subText }: EmptyListProperties) => {
           height={1000}
         />
       </aside>
-<<<<<<< HEAD
       <h5 className="text-neutral-dark-2 mx-auto w-[324px] text-center text-[20px] font-semibold leading-normal sm:w-full sm:text-[28px]">
         {mainText}
       </h5>
       <h6 className="text-neutral-dark-2 mx-auto w-[315px] text-center text-[18px] font-normal leading-normal sm:w-full sm:text-[20px]">
-=======
-      <h5 className="mx-auto w-[324px] text-center text-[20px] font-semibold leading-normal text-[#525252] sm:w-full">
-        {mainText}
-      </h5>
-      <h6 className="mx-auto w-[315px] text-center text-[18px] font-normal leading-normal text-[#525252] sm:w-full">
->>>>>>> 6d984f635f90a408562a2fb74cb152a1fb821393
         {subText}
       </h6>
     </section>
