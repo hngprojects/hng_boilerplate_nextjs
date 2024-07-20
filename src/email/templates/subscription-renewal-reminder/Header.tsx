12cb11f8130335ad779ec41a5a9d2ce4b6b5ebd0
callmestiles <callmestiless@gmail.com>
2024-07-20 11:43:07 +0300

    feat: design footer, header and subscription email template; use react-email and tailwind css

---
+++ src/email/templates/subscription-renewal-reminder/Header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-center bg-[rgba(225,214,214,0.4)] p-16">
+      <Image src="/icon-logo.svg" alt="Logo" width={18} height={18} />
+      <h1 className="ml-4 text-[2.4rem] font-semibold text-[#121a26]">
+        Boilerplate.
+      </h1>
+    </header>
+  );
+}
