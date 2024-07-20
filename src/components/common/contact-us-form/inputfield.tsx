4aca8b0a3bcc260289496cdd398d14afae0cc9b0
Glad Chukwu <nellg.gc@gmail.com>
2024-07-20 05:36:20 -0700

    fix: test user contact inquiry form

--- src/components/common/contact-us-form/inputfield.tsx
+++ src/components/common/contact-us-form/inputfield.tsx
@@ -21,7 +21,7 @@ const InputField: React.FC<InputFieldProps> = ({ value, type, onChange, placehol
       value={value}
       onChange={onChange}
       placeholder={placeholder}
-      className="shadow appearance-none border rounded w-full py-5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      className="appearance-none border rounded w-full py-5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
     />
   </div>
 );
