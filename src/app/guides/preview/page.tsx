3539435fdf58186c65be62caea239ab0c969d2fb
Godwin Archibong <archibong_godwin@yahoo.com>
2024-07-20 19:08:47 +0100

    [Feature]: Editor Block Component [Description]: 1. create an editor block component, 2. add a toolbar that changes state based on client interaction, 3. create an editor context for the template [Issue Number]: [Feature]: Email Templates > SuperAdmin Email Management Dashboard >HTML Template Editor Block Component #205 [Additional Notes]:

---
+++ src/app/guides/preview/page.tsx
@@ -0,0 +1,9 @@
+import PlaceholderPage from "./PlaceholderPage";
+
+export default function TemplateEditorPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <PlaceholderPage />
+    </div>
+  );
+}
