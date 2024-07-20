f2e13363862586e5310ce827b2ea902ebbc70f82
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2024-07-20 19:10:45 +0100

    feat: layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Layout from "~/components/layouts";
+import Hero from "~/components/layouts/homepage/Hero";
+import HowItWorks from "~/components/layouts/homepage/HowItWorks";
+import PerfectFit from "~/components/layouts/homepage/PerfectFit";
+import Testimonials from "~/components/layouts/homepage/Testimonials";
+import UserSection from "~/components/layouts/homepage/UserSection";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <Layout>
+      <Hero />
+      <UserSection />
+      <HowItWorks />
+      <Testimonials />
+      <PerfectFit />
+    </Layout>
+  );
 }
