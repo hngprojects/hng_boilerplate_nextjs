9995409cfcd75e2da3dec460a11e49212a62faa7
Kingsley <kingsleyakwa@gmail.com>
2024-07-20 09:34:24 +0100

    fix: update test file

--- src/components/modals/PasswordSuccessfulModal/PasswordSuccessfulModal.tsx
+++ src/components/modals/PasswordSuccessfulModal/PasswordSuccessfulModal.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import React from "react";
-import { Dialog, DialogContent, DialogOverlay, DialogTitle, DialogDescription } from "../../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogTitle,
+  DialogDescription,
+} from "../../ui/dialog";
 import CustomButton from "~/components/common/Button/button";
 
 interface ModalProperties {
@@ -9,21 +15,26 @@ interface ModalProperties {
   onClose: () => void;
 }
 
-const PasswordSuccessfulModal: React.FC<ModalProperties> = ({ show, onClose }) => {
+const PasswordSuccessfulModal: React.FC<ModalProperties> = ({
+  show,
+  onClose,
+}) => {
   return (
     <Dialog open={show} onOpenChange={onClose}>
-      <DialogOverlay 
-      data-testid = 'overlay'
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" />
-      <DialogContent 
-        className=" mx-10 rounded-md bg-white p-6 shadow-md" 
+      <DialogOverlay
+        data-testid="overlay"
+        className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      />
+      <DialogContent
+        className="mx-10 rounded-md bg-white p-6 shadow-md"
         onClick={(event) => event.stopPropagation()}
       >
-        <DialogTitle className=" text-lg font-semibold">
+        <DialogTitle className="text-lg font-semibold">
           Password Successfully Updated!
         </DialogTitle>
         <DialogDescription className="text-sm font-normal text-muted-foreground">
-          Your password has been successfully updated! You can now log in with your new password.
+          Your password has been successfully updated! You can now log in with
+          your new password.
         </DialogDescription>
         <div className="flex justify-end">
           <div onClick={onClose}>
