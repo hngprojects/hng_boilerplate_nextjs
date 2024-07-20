9e09a2f0b3b4709da0f1b9534b7485c96d5fbd8f
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-07-20 18:27:39 +0100

    feat: Update deactivation component

--- src/email/templates/incoming-account-deactivation/no-image.tsx
+++ src/email/templates/incoming-account-deactivation/no-image.tsx
@@ -29,14 +29,43 @@ export default function Email(properties: IProperties) {
             <Preview>{`Incoming Account Deactivation for ${name}`}</Preview>
 
             <Tailwindwrapper>
-                <Img src="cat.jpg" alt="Deactivation logo" width="300" height="300" />
-                <Heading className="">Account Deactivation In  <span className="">Two Days!</span></Heading>
-                <Button
-                    href="https://example.com"
-                    className="bg-black px-5 py-3 text-white"
-                >
-                    Click me
-                </Button>
+                <div className=" flex flex-col justify-center items-center ">
+                    <Img src="cat.jpg" alt="Deactivation logo" className="w-[16.7%] " />
+                    <Heading className="text-center">Account Deactivation In  <span className="">Two Days!</span></Heading>
+                    <main>
+                        <h2 className="text-gray-700 dark:text-gray-200">Hi {name},</h2>
+                        <p>
+                            We hope this email finds you well. We noticed that you haven't logged into your Boilerplate account for quite some time. As part of our ongoing efforts to maintain a secure and efficient service, we will be deactivating inactive accounts.
+                        </p>
+
+                        <section>
+                            <span>Your deactivation deatails:</span>
+                            <ul>
+                                <li>
+                                    <span>Account Email: </span>
+                                    <span>johndoe@gmail.com</span>
+                                </li>
+                                <li>
+                                    <span>Last Active: </span>
+                                    <span>17th June, 2024 / 11:56pm</span>
+                                </li>
+                                <li>
+                                    <span>Deactivation Date: </span>
+                                    <span>20th July, 2024 / 11:56pm</span>
+                                </li>
+                            </ul>
+                        </section>
+                        <span>To keep your account active, simply log in before the deactivation date. However, if you no longer wish to use your account, no further action is needed on your part.</span>
+                    </main>
+
+                    <Button
+                        href="https://example.com"
+                        className="bg-black px-5 py-3 text-white"
+                    >
+                        Access My Account
+                    </Button>
+                </div>
+               
             </Tailwindwrapper>
         </Html>
     );
