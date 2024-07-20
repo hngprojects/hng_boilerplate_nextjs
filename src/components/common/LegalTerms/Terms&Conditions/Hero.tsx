c02f1df41746a11cc01ca0912f0b2dc7dc26bf2d
Jibola Paul <harjibbolar26@gmail.com>
2024-07-20 01:36:15 +0100

    feat: use css global variables, add test file for components and links, run test to pass

--- src/components/common/LegalTerms/Terms&Conditions/Hero.tsx
+++ src/components/common/LegalTerms/Terms&Conditions/Hero.tsx
@@ -9,13 +9,13 @@ export type HeroType = {
 
 const Hero: NextPage<HeroType> = () => {
   return (
-    <div className="w-full h-full overflow-hidden  ">
-      <div className="w-full relative top-[0px] right-[0px] bottom-[0px] left-[0px] bg-black z-[1]">
+    <div className="w-full h-full overflow-hidden">
+      <div className="w-full relative top-[0px] right-[0px] bottom-[0px] left-[0px] bg-default z-[1]">
         <section className="gap-[10px] max-w-full z-[2] text-center py-16 px-12 sm:py-24 sm:px-12 md:py-30 md:px-18 lg:py-36 lg:px-20  xl:px-[252px] xl:py-[102px]">
-          <div className="self-stretch relative font-bold md:text-[60px] sm:text-[48px] max-sm:text-[28px] text-white">
+          <div className="self-stretch relative font-bold md:text-[60px] sm:text-[48px] max-sm:text-[28px] text-default-foreground">
             Terms and Conditions
           </div>
-          <div className="self-stretch relative md:text-[28px] max-md:text-[22px] max-sm:text-[18px] text-white font-[400]">
+          <div className="self-stretch relative md:text-[28px] max-md:text-[22px] max-sm:text-[18px] text-default-foreground font-[400]">
             Achieve your dreams with us today
           </div>
         </section>
--- src/components/common/LegalTerms/Terms&Conditions/Main.tsx
+++ src/components/common/LegalTerms/Terms&Conditions/Main.tsx
@@ -8,17 +8,37 @@ import IntellectualProperty from "./IntellectualProperty";
 import TableOfContents from "./TableOfContents";
 import UserObligations from "./UserObligations";
 
+const formattedDate = (date: Date) => {
+  const day = date.getDate();
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
+
+  const dateSuffix = (day: number) => {
+    if (day > 3 && day < 21) return 'th';
+    switch (day % 10) {
+        case 1: return 'st';
+        case 2: return 'nd';
+        case 3: return 'rd';
+        default: return 'th';
+      }
+    }
+
+    return `${day}${dateSuffix(day)} ${month}, ${year}`;
+}
+
 const Main = () => {
+  const lastUpdate = new Date('2024-07-20');
   return (
     <section className=" max-w-full text-left mx-[222px] max-sm:mx-6 max-lg:mx-12 max-xl:mx-20 my-[64px]">
-      <Breadcrumb />
+
+      <Breadcrumb /> {/*Breadcrumb component to be replaced*/} 
       <div className="flex lg:flex-row flex-col-reverse items-start justify-between pb-10 max-w-full">
-        <div className="lg:w-[612px]">
+        <div className="lg:w-[612px] text-neutral-dark-1">
           <div
             className="self-stretch max-w-full xl:w-[612px]"
             id="introduction"
           >
-            <div className="mb-[10px] self-stretch relative text-inherit font-bold sm:text-[28px] text-2xl">
+            <div className="mb-[10px] self-stretch relative font-bold sm:text-[28px] text-2xl">
               Introduction
             </div>
             <div className="self-stretch relative text-base inline-block">
@@ -28,22 +48,22 @@ const Main = () => {
               not agree, please do not use our website..
             </div>
           </div>
-          <div id="user-obligations">
+          <div id="user-obligations" className="text-neutral-dark-1 mb-6">
             <UserObligations />
           </div>
-          <div id="acceptable-use-policy">
+          <div id="acceptable-use-policy" className="text-neutral-dark-1 mb-6">
             <AcceptableUse />
           </div>
-          <div id="intellectual-property">
+          <div id="intellectual-property" className="text-neutral-dark-1 mb-6">
             <IntellectualProperty />
           </div>
-          <div id="disclaimer">
+          <div id="disclaimer" className="text-neutral-dark-1 mb-6">
             <Discclaimer />
           </div>
-          <div id="governing-law">
+          <div id="governing-law" className="text-neutral-dark-1 mb-6">
             <GoverningLaws />
           </div>
-          <div className="self-stretch" id="changes-to-terms">
+          <div className="self-stretch text-neutral-dark-1 mb-6" id="changes-to-terms">
             <h2 className="mt-0 self-stretch relative text-inherit font-bold font-inherit sm:text-[28px] text-2xl">
               Changes to Terms
             </h2>
@@ -56,7 +76,7 @@ const Main = () => {
             </div>
           </div>
           <div
-            className="self-stretch flex flex-col items-start justify-start gap-[10px]"
+            className="self-stretch flex flex-col items-start justify-start gap-[10px] text-neutral-dark-1 mb-6"
             id="contact-information"
           >
             <h2 className="mt-0 self-stretch relative text-inherit font-bold font-inherit sm:text-[28px] text-2xl">
@@ -69,20 +89,20 @@ const Main = () => {
             </div>
           </div>
           <div
-            className="self-stretch flex flex-col items-start justify-start gap-[10px]"
+            className="self-stretch flex flex-col items-start justify-start gap-[10px] text-neutral-dark-1"
             id="last-updated"
           >
             <h2 className="mt-0 self-stretch relative text-inherit font-bold font-inherit sm:text-[28px] text-2xl">
               Last Updated
             </h2>
             <div className="mb-[10px] text-base">
-              These Terms and Conditions were last updated on [Date]. Please
+              These Terms and Conditions were last updated on [{formattedDate(lastUpdate)}]. Please
               review them periodically for any changes.
             </div>
           </div>
-          <div className="mb-[10px] text-base">
+          <div className="mb-[10px] text-base text-neutral-dark-1">
             For more information about our privacy practices, please visit our{" "}
-            <Link href={"/"} className="text-[#F97316]">
+            <Link href={"/privacy_policy"} className="text-primary underline">
               Privacy Policy page.{" "}
             </Link>
           </div>
