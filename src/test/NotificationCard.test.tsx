e5741d4a3b27072478299d752710a70c06329773
Codegirlemem <eduokuemem@gmail.com>
2024-07-20 16:28:26 +0100

    feat: create notification card component

--- src/test/NotificationCard.test.tsx
+++ src/test/NotificationCard.test.tsx
@@ -18,86 +18,38 @@ function renderComponents(preview: NotificationPreview[] = []) {
   return {
     previewCount: screen.getByTestId("unreadMessageCount"),
     previewBody: screen.getByTestId("previewBody"),
-    CTAButton: screen.getByTestId("CTAButton"),
+    CTAButton: screen.getByRole("button"),
     switchButton: screen.getByRole("switch"),
     cardTitle: screen.getByRole("heading"),
     cardContainer: screen.getByTestId("cardContainer"),
   };
 }
 
+// TESTS WHEN THERE ARE NO NOTIFICATIONS
 describe("notificationCard", () => {
-  // display notification title
   it("should display notification title", () => {
     expect.assertions(2);
 
     const { cardTitle } = renderComponents();
-    expect(cardTitle).toBeInTheDocument();
+
+    expect(screen.getByRole("heading")).toBeInTheDocument();
     expect(cardTitle).toHaveTextContent("Notifications");
   });
 
-  //   should have push notification button unchecked
+  it("should display 'no unread message if notificationPreview array is empty", () => {
+    expect.assertions(1);
+
+    const { previewCount } = renderComponents();
+
+    expect(previewCount).toHaveTextContent(/0 unread messages/i);
+  });
+
   it("should render push notification button", () => {
     expect.assertions(1);
 
     const { switchButton } = renderComponents();
+
     expect(switchButton).toBeInTheDocument();
-  });
-
-  it("should render disabled CTA button if no unread messages", () => {
-    expect.assertions(2);
-
-    const { CTAButton } = renderComponents();
-
-    expect(CTAButton).toBeInTheDocument();
-    expect(CTAButton).toBeDisabled();
-  });
-
-  it("should enable CTA button  if there are unread messages", () => {
-    expect.assertions(2);
-
-    const previewMessages: NotificationPreview[] = [
-      { header: "Check mail", time: "1 hour ago" },
-      { header: "Sign up for offer", time: "2 hours ago" },
-      { header: "Register for event", time: "1 hour ago" },
-    ];
-
-    const { CTAButton } = renderComponents(previewMessages);
-
-    expect(CTAButton).toBeInTheDocument();
-    expect(CTAButton).toBeEnabled();
-  });
-});
-
-// TESTING PREVIEW MESSAGE DISPLAY IN CARD BODY
-describe("notificationCard Messages", () => {
-  const previewMessages: NotificationPreview[] = [
-    { header: "Check mail", time: "1 hour ago" },
-    { header: "Sign up for offer", time: "2 hours ago" },
-    { header: "Register for event", time: "1 hour ago" },
-  ];
-
-  describe.each(previewMessages)("notificationCard with preview", (message) => {
-    it(`should display preview with header "${message.header}" and time "${message.time}"`, () => {
-      expect.assertions(4);
-
-      const index = previewMessages.indexOf(message);
-
-      render(
-        <NotificationCard
-          notificationsPreview={previewMessages}
-          unreadCount={previewMessages.length}
-        />,
-      );
-
-      const header = screen.getByTestId(`previewHeader${index}`);
-      const time = screen.getByTestId(`previewTime${index}`);
-
-      expect(header).toBeInTheDocument();
-      expect(header).toHaveTextContent(message.header);
-
-      expect(time).toBeInTheDocument();
-      expect(time).toHaveTextContent(message.time);
-    });
   });
 
   it("should have no message in preview card body if no notification", () => {
@@ -107,23 +59,23 @@ describe("notificationCard Messages", () => {
     expect(previewBody).toBeEmptyDOMElement();
   });
 
-  it("should be on screen at smaller screen size", () => {
-    expect.assertions(0);
-    // Set viewport to small screen size
-    global.innerWidth = 320;
-    global.dispatchEvent(new Event("resize"));
+  it("should disabled CTA button if no unread messages", () => {
+    expect.hasAssertions();
+
+    const { CTAButton } = renderComponents();
+
+    expect(CTAButton).toBeInTheDocument();
+    expect(CTAButton).toBeDisabled();
   });
 });
 
-// TEST FOR NUMBER OF UNREAD  MESSAGES IN PREVIEW ARRAY
-describe("notificationCard unread mesage count", () => {
-  it("should display 'no unread message if notificationPreview array is empty", () => {
-    expect.assertions(1);
-
-    const { previewCount } = renderComponents();
-
-    expect(previewCount).toHaveTextContent(/0 unread messages/i);
-  });
+// TESTING WHEN THERE ARE NOTIFICATIONS
+describe("notificationCard Messages", () => {
+  const previewMessages: NotificationPreview[] = [
+    { header: "Check mail", time: "1 hour ago" },
+    { header: "Sign up for offer", time: "2 hours ago" },
+    { header: "Register for event", time: "1 hour ago" },
+  ];
 
   it("should display '1 unread message if notificationPreview array has one message", () => {
     expect.assertions(1);
@@ -150,13 +102,52 @@ describe("notificationCard unread mesage count", () => {
 
     expect(previewCount).toHaveTextContent(/3 unread messages/i);
   });
+
+  describe.each(previewMessages)("notificationCard with preview", (message) => {
+    it(`should display preview with header "${message.header}" and time "${message.time}"`, () => {
+      expect.assertions(4);
+
+      const index = previewMessages.indexOf(message);
+
+      render(
+        <NotificationCard
+          notificationsPreview={previewMessages}
+          unreadCount={previewMessages.length}
+        />,
+      );
+
+      const header = screen.getByTestId(`previewHeader${index}`);
+      const time = screen.getByTestId(`previewTime${index}`);
+
+      expect(header).toBeInTheDocument();
+      expect(header).toHaveTextContent(message.header);
+
+      expect(time).toBeInTheDocument();
+      expect(time).toHaveTextContent(message.time);
+    });
+  });
+
+  it("should enable CTA button  if there are unread messages", () => {
+    expect.assertions(2);
+
+    const { CTAButton } = renderComponents(previewMessages);
+
+    expect(CTAButton).toBeInTheDocument();
+    expect(CTAButton).toBeEnabled();
+  });
 });
 
-// TEST FOR PRESENCE OF REQUIRED ELEMENTS ON SMALLER SCREEN SIZE
+// TEST FOR PRESENCE OF REQUIRED ELEMENTS ON SMALLER SCREEN SIZE WITH OR WITHOUT NOTIFICATIONS
 describe("notificationCard responsiveness", () => {
+  const previewMessages: NotificationPreview[] = [
+    { header: "Check mail", time: "1 hour ago" },
+    { header: "Sign up for offer", time: "2 hours ago" },
+    { header: "Register for event", time: "1 hour ago" },
+  ];
+
   it("should render all required elements correctly on smaller screen sizes", () => {
     expect.assertions(5);
-    // Set viewport to small screen size
+
     global.innerWidth = 320;
     global.dispatchEvent(new Event("resize"));
 
@@ -169,14 +160,6 @@ describe("notificationCard responsiveness", () => {
     expect(switchButton).toBeInTheDocument();
     expect(cardTitle).toBeInTheDocument();
   });
-});
-
-describe("notificationCard message responsiveness", () => {
-  const previewMessages: NotificationPreview[] = [
-    { header: "Check mail", time: "1 hour ago" },
-    { header: "Sign up for offer", time: "2 hours ago" },
-    { header: "Register for event", time: "1 hour ago" },
-  ];
 
   describe.each(previewMessages)("notificationCard with preview", (message) => {
     it(`should display preview with header "${message.header}" and time "${message.time}"`, () => {
@@ -205,7 +188,7 @@ describe("notificationCard message responsiveness", () => {
   });
 });
 
-// TEST FOR BRAND STYLE for
+// TEST FOR BRAND STYLE ACCURACY
 describe("notificationCard style ", () => {
   it("should have the correct card style ", () => {
     expect.assertions(1);
@@ -213,7 +196,7 @@ describe("notificationCard style ", () => {
     const { cardContainer } = renderComponents();
 
     expect(cardContainer).toHaveClass(
-      "bg-card rounded-lg border text-card-foreground shadow-sm",
+      "bg-card w-fit text-card-foreground sm:w-[380px]",
     );
   });
 
@@ -222,10 +205,10 @@ describe("notificationCard style ", () => {
 
     const { CTAButton } = renderComponents();
 
-    expect(CTAButton).toHaveClass("w-full bg-[#F97316]");
+    expect(CTAButton).toHaveClass("bg-primary w-full");
   });
 
-  it("should have the correct CTAbutton color ", () => {
+  it("should have the correct switch color ", () => {
     expect.assertions(1);
 
     const { switchButton } = renderComponents();
