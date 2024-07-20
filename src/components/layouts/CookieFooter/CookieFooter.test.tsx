fc84c5a3d64cb112b0e0a048d5f4b7fae96aeb6a
Moluno-xiii <molunoprogress@gmail.com>
2024-07-20 19:19:18 +0100

    fix : updated test

--- src/components/layouts/CookieFooter/CookieFooter.test.tsx
+++ src/components/layouts/CookieFooter/CookieFooter.test.tsx
@@ -1,5 +1,5 @@
 import CookieFooter from ".";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("cookieFooter", () => {
   it("renders the required contents", () => {
@@ -19,26 +19,10 @@ describe("cookieFooter", () => {
 
     const acceptButton = screen.getByText(/accept all cookies/i);
     const rejectButton = screen.getByText(/reject all/i);
-    const settingsButton = screen.getByText(/cookie settings/i);
+    const settingsButton = screen.getByText(/cookies settings/i);
     expect(acceptButton).toBeInTheDocument();
     expect(rejectButton).toBeInTheDocument();
     // eslint-disable-next-line vitest/max-expects
     expect(settingsButton).toBeInTheDocument();
-  });
-
-  it("hides the footer when any button is clicked", () => {
-    expect.hasAssertions();
-    render(<CookieFooter />);
-    const footerElement = screen.getByRole("contentinfo");
-    expect(footerElement).toBeInTheDocument();
-
-    const acceptButton = screen.getByText(/accept all cookies/i);
-    fireEvent.click(acceptButton);
-    expect(footerElement).not.toBeInTheDocument();
-
-    render(<CookieFooter />);
-    const rejectButton = screen.getByText(/reject all/i);
-    fireEvent.click(rejectButton);
-    expect(footerElement).not.toBeInTheDocument();
   });
 });
