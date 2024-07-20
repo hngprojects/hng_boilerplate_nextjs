476285c7b37828a2ea92e6c4c0bdf885346e7909
Marvellous-Udoye <155150860+Marvellous-Udoye@users.noreply.github.com>
2024-07-20 23:46:51 +0100

    Minor Edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,60 @@
+import Image from "next/image";
+import styles from '~/app/globals.module.css';
+import { error } from "console";
 import { detailedPayment } from "~/app/dummyData";
+import { promises } from "dns";
+import Checked from '~/components/images/checked.svg'
 
 export interface detailedPaymentPlan {
-  name: string;
-  description: string;
-  price: number;
-  features: string[];
-  projectMgt: string[];
-  sharing: string[];
-  support: string[];
-  current_plan_description: string;
-  btn: string;
+  name: string,
+  description: string,
+  price: number,
+  features: string[],
+  projectMgt: string[],
+  sharing: string[],
+  support: string[]
+  current_plan_description: string,
+  btn: string,
 }
 
 export const fetchPaymentPlan = async (): Promise<detailedPaymentPlan[]> => {
   try {
-    const response = await fetch("");
+    const response = await fetch('')
 
     if (!response.ok) {
-      throw new Error("Failed to Fetch Plans");
+      throw new Error('Failed to Fetch Plans')
     }
 
-    const data: detailedPaymentPlan[] = await response.json();
-    return data;
-  } catch {
-    return detailedPayment;
+    const data: detailedPaymentPlan[] = await response.json()
+    return data
+  } catch (error) {
+    return detailedPayment
   }
-};
+
+}
 
 export default async function PaymentPlan() {
-  const paymentPlan = await fetchPaymentPlan();
+  const paymentPlan = await fetchPaymentPlan()
 
   return (
     <main className="min-h-svh max-w-[989px] mx-auto mt-[102px]">
       <article className={styles.main}>
-        <p className="font-[600] text-[24px] text[#0A0A0A] pl-[17px]">
-          Current Plan
-        </p>
+        <p className="font-[600] text-[24px] text[#0A0A0A] pl-[17px]">Current Plan</p>
 
         <div className="pl-[17px] pt-[24px] pb-[22px] pr-[94px] bg-[#FFF8F2] rounded-[12px] my-[24px]">
-          <p className="text-[#0A0A0A] text-[20px] font-[600] leading-normal">
-            Current Plan
-          </p>
-          <p className="text-[14px] font-[400] md:whitespace-nowrap">
-            Your account is on a free 90-day trial of our free plan, through
-            September 27th. Upgrade anytime to stay on this plan when your trial
-            ends
-          </p>
+          <p className="text-[#0A0A0A] text-[20px] font-[600] leading-normal">Current Plan</p>
+          <p className="text-[14px] font-[400] md:whitespace-nowrap">Your account is on a free 90-day trial of our free plan, through September 27th. Upgrade anytime to stay on this plan when your trial ends</p>
           <p className="text-[#525252] text-[14px] font-[400]">$0/month</p>
         </div>
 
         <div className="flex pl-[17px] w-full sm:flex sm:flex-cols">
-          <p className="text-[16px] font-[600] mt-[15px] max-w-[130px]">
-            Subscribe to your desired plan
-          </p>
+          <p className="text-[16px] font-[600] mt-[15px] max-w-[130px]">Subscribe to your desired plan</p>
           <div className="grid grid-cols-4 w-full sm:flex sm:flex-cols">
             {paymentPlan.map((plan, index) => (
               <div key={index} className={styles.paymentCards}>
                 <p className="text-[16px] font-[600]">{plan.name}</p>
-                <p className="text-[25px] font-[500]">
-                  {`$${plan.price}`}{" "}
-                  <span className="text-[14px] font-[400] text-[#525252]">
-                    /month
-                  </span>
-                </p>
-                <p className="text-[14px] font-[400] text-[#525252] w-[160px]">
-                  {plan.description}
-                </p>
+                <p className="text-[25px] font-[500]">{`$${plan.price}`} <span className="text-[14px] font-[400] text-[#525252]">/month</span></p>
+                <p className="text-[14px] font-[400] text-[#525252] w-[160px]">{plan.description}</p>
                 <button className={styles.paymentBtn}>{plan.btn}</button>
               </div>
             ))}
@@ -86,9 +75,7 @@ export default async function PaymentPlan() {
           </div>
         </div>
 
-        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px] leading-[19.36px]">
-          Project Management
-        </p>
+        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px] leading-[19.36px]">Project Management</p>
         <div className={styles.prodMgt}>
           <div className={styles.prodMgtPlanTitle}>
             <p>Projects</p>
@@ -107,9 +94,7 @@ export default async function PaymentPlan() {
           ))}
         </div>
 
-        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px] leading-[19.36px]">
-          Sharing and collaboration
-        </p>
+        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px] leading-[19.36px]">Sharing and collaboration</p>
         <div className={styles.prodMgt}>
           <div className={styles.prodMgtPlanTitle}>
             <p>Integration</p>
@@ -122,20 +107,8 @@ export default async function PaymentPlan() {
               <div className={styles.prodMgtPlanb}>
                 {plan.projectMgt.map((projectMt, projectMtIndex) => (
                   <div className="py-[26px] flex justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                    >
-                      <path
-                        d="M20 6L9 17L4 12"
-                        stroke="#0A0A0A"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      <path d="M20 6L9 17L4 12" stroke="#0A0A0A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                   </div>
                 ))}
@@ -144,9 +117,7 @@ export default async function PaymentPlan() {
           ))}
         </div>
 
-        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px] leading-[19.36px]">
-          Management and security
-        </p>
+        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px] leading-[19.36px]">Management and security</p>
         <div className={styles.prodMgt}>
           <div className={styles.prodMgtPlanTitle}>
             <p>Team Security</p>
@@ -158,20 +129,8 @@ export default async function PaymentPlan() {
               <div className={styles.prodMgtPlan}>
                 {plan.sharing.map((share, shareIndex) => (
                   <div className="py-[26px] flex justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                    >
-                      <path
-                        d="M20 6L9 17L4 12"
-                        stroke="#0A0A0A"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      <path d="M20 6L9 17L4 12" stroke="#0A0A0A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                   </div>
                 ))}
@@ -180,9 +139,7 @@ export default async function PaymentPlan() {
           ))}
         </div>
 
-        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px]">
-          Support
-        </p>
+        <p className="font-[700] text-[16px] bg-[#FFF8F2] py-[28px] pl-[10px] pr-[39px]">Support</p>
         <div className={styles.prodMgt}>
           <div className={styles.prodMgtPlanTitle}>
             <p>Priority Support</p>
@@ -193,20 +150,8 @@ export default async function PaymentPlan() {
               <div className={styles.prodMgtPlan}>
                 {plan.support.map((sup, supIndex) => (
                   <div className="py-[26px] flex justify-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                    >
-                      <path
-                        d="M20 6L9 17L4 12"
-                        stroke="#0A0A0A"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      <path d="M20 6L9 17L4 12" stroke="#0A0A0A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                   </div>
                 ))}
@@ -214,6 +159,7 @@ export default async function PaymentPlan() {
             </div>
           ))}
         </div>
+
       </article>
     </main>
   );
