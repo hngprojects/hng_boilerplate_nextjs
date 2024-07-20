47a863aa5bb2b2a988c8714359250afcbbc3c1fb
Abd'Quadri Jinad <abdjinadquadri@gmail.com>
2024-07-20 13:52:10 +0100

    Ran eslint

--- src/components/common/FormPage/EyesClosedIcon.tsx
+++ src/components/common/FormPage/EyesClosedIcon.tsx
@@ -1,4 +1,3 @@
-
 const EyesClosedIcon = () => {
   return (
     <svg
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -36,20 +36,25 @@ const buttonVariants = cva(
   },
 );
 
-export interface ButtonProps
+export interface ButtonProperties
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
+<<<<<<< HEAD
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
+=======
+const Button = React.forwardRef<HTMLButtonElement, ButtonProperties>(
+  ({ className, variant, size, asChild = false, ...properties }, reference) => {
+>>>>>>> 2222bd3 (Ran eslint)
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
+        ref={reference}
+        {...properties}
       />
     );
   },
