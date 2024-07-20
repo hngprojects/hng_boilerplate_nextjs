8c6be5191f2672142e141593454418231f1c1ff2
Abd'Quadri Jinad <abdjinadquadri@gmail.com>
2024-07-20 13:36:21 +0100

    Changed the custom modal component to chadcn component, changed all static tailwind classes

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,10 +4,10 @@ const Page = () => {
   return (
     <div className="w-full space-y-6 px-[5%] pt-4 md:w-2/3">
       <div className="">
-        <h3 className="self-stretch text-2xl font-semibold capitalize text-[#141414]">
+        <h3 className="text-2xl font-semibold capitalize">
           Password settings
         </h3>
-        <p className="self-stretch text-sm font-normal text-[#434343]">
+        <p className="self-stretch text-sm font-normal opacity-80">
           Update password for enhanced account security
         </p>
       </div>
