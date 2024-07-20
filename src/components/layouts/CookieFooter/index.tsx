00f75553c1b4c637d918b297f37e7f7e2e522c5c
Moluno-xiii <molunoprogress@gmail.com>
2024-07-20 09:58:51 +0100

    fix : updated test cases

--- src/components/layouts/CookieFooter/index.tsx
+++ src/components/layouts/CookieFooter/index.tsx
@@ -2,6 +2,13 @@
 import CustomButton from "~/components/common/Button/button";
 
 const CookieFooter: React.FC = () => {
+  const [isCookieFooterOpen, setIsCookieFooterOpen] = useState(true);
+
+  const handleCloseFooter = () => {
+    setIsCookieFooterOpen(false);
+  };
+
+  if (!isCookieFooterOpen) return;
   return (
     <footer className="fixed bottom-0 left-0 right-0 z-50 w-[100vw] bg-background text-start text-neutral-dark-1 md:text-foreground">
       <div className="mx-auto flex max-w-[1280px] flex-col gap-x-[25px] gap-y-4 px-6 py-5 md:py-10 lg:flex-row lg:justify-center">
@@ -14,12 +21,12 @@ const CookieFooter: React.FC = () => {
           </span>
         </p>
         <ul className="flex flex-row flex-wrap items-end gap-x-4 gap-y-2">
-          <li className="md:order-3">
+          <li onClick={handleCloseFooter} className="md:order-3">
             <CustomButton size="default" variant="primary">
               Accept All Cookies
             </CustomButton>
           </li>
-          <li className="md:order-2">
+          <li onClick={handleCloseFooter} className="md:order-2">
             <CustomButton variant="primary" size="default">
               Reject All
             </CustomButton>
