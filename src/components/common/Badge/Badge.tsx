772de0cdee8cb619b01159ddb52e19a16b1f7d0d
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-20 20:58:00 +0100

    docs: Update the style guide with the badge component

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -14,8 +14,7 @@
  * - Import the component and its props ineterface
  *
  * ```
- * import Badge from "~/components/common/Badge/Badge";
- * import BadgeInterface from "~/components/common/Badge/BadgeInterface";
+ * import Badge, { BadgeInterface } from "~/components/common/Badge/Badge";
  * ```
  * - Option1: Define the props object, render the component, and spread the object as props to it
  *
@@ -43,18 +42,28 @@ export interface BadgeInterface {
 
 const Badge = ({ label, icon, variant }: BadgeInterface) => {
   const badgeTextVariants = {
-    default: "badge-default",
-    primary: "badge-primary",
-    success: "badge-success",
-    error: "badge-error",
+    default: "text-[hsl(var(--default))]",
+    primary: "text-[hsl(var(--primary))]",
+    success: "text-[hsl(var(--success))]",
+    error: "text-[hsl(var(--error))]",
+  };
+  const badgeBackgroundVariants = {
+    default: "bg-[hsl(222,47%,11%)]",
+    primary: "bg-[hsl(25,95%,53%)]",
+    success: "bg-[hsl(102,51%,52%)]",
+    error: "bg-[hsl(0,72%,51%)]",
   };
 
   return (
     <div
-      className={`${badgeTextVariants[variant]} flex·max-w-max·items-center·gap-[6px]·rounded-[16px]·py-[2px]·pl-[6px]·pr-2·text-[0.75rem]·font-[500]·capitalize·leading-[18px]·md:py-[2px]·md:pl-[8px]·md:pr-[10px]·md:text-[0.875rem]·lg:py-1·lg:pl-[10px]·lg:pr-3·lg:leading-[20px]`}
+      className={`${badgeBackgroundVariants[variant]} flex max-w-max rounded-[16px] bg-opacity-10`}
     >
-      {icon && icon}
-      <span>{label}</span>
+      <div
+        className={`${badgeTextVariants[variant]} flex gap-[6px] py-[2px] pl-[6px] pr-2 text-[0.75rem] font-[500] capitalize leading-[18px] md:py-[2px] md:pl-[8px] md:pr-[10px] md:text-[0.875rem] lg:py-1 lg:pl-[10px] lg:pr-3 lg:leading-[20px]`}
+      >
+        {icon && icon}
+        <span>{label}</span>
+      </div>
     </div>
   );
 };
