8ac66074811a640cf3d126dfc217080088697859
Marvelous <marvelous.udugbesi@icloud.com>
2024-07-20 19:33:30 +0100

    update page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import AboutUsPage from "~/components/layouts/AboutUs/AboutUs";
+import Hero from "~/components/layouts/homepage/Hero";
+import HowItWorks from "~/components/layouts/homepage/HowItWorks";
+import PerfectFit from "~/components/layouts/homepage/PerfectFit";
+import Testimonials from "~/components/layouts/homepage/Testimonials";
+import UserSection from "~/components/layouts/homepage/UserSection";
 
 export default function Home() {
   return (
@@ -8,7 +12,6 @@ export default function Home() {
       <HowItWorks />
       <Testimonials />
       <PerfectFit />
-      
     </>
   );
 }
