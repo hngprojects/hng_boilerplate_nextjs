b98ec663640f6dd0714a6de6493948f87c810f77
= <=>
2024-07-21 13:16:27 +0100

    feat: Make this a component for easy redability

---
+++ src/components/common/PricePlanCard/PricePlanBenefit.tsx
@@ -0,0 +1,10 @@
+import PricePlanCheckMark from "./PricePlanCheckMark";
+
+export default function PricePlanBenefit() {
+  return (
+    <div className="flex items-center gap-3">
+      <PricePlanCheckMark />
+      <p>2 Projects</p>
+    </div>
+  );
+}
