34011814a4bdf8c1f27276deb5487f67bcecd61f
Peter <olasunkanmifinesse@gmail.com>
2024-07-21 20:23:46 +0100

    Removed file that are not my work that is giving me error

--- src/env.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable unicorn/prevent-abbreviations */
-import { createEnv } from "@t3-oss/env-nextjs";
-
-export const env = createEnv({
-  server: {},
-  client: {},
-  //   For Next.js >= 13.4.4, you only need to destructure client variables:
-  experimental__runtimeEnv: {},
-});
