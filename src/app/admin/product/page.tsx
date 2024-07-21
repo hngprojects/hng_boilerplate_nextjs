8058da135ff0000ccdcfb76d07360d2cbd1c0ee2
omotoso eniola <omotosoeniola2@gmail.com>
2024-07-21 05:16:49 +0100

    feat(HNG-146):Create a product table list.

---
+++ src/app/admin/product/page.tsx
@@ -0,0 +1,12 @@
+import ProductFilter from "~/components/common/ProductFilter";
+import TableData from "~/components/common/Table";
+
+export default function Page() {
+  return (
+    <main className="">
+      <ProductFilter />
+      <TableData />
+
+    </main>
+  );
+}
