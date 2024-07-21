32f7271c8973e57b9924bec1bc9c90c40fd0b156
Vxrcel <skyeparagon@gmail.com>
2024-07-21 08:29:58 +0100

    Update: tailwind wrapper for react-email Feat: Admin Products page

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import AdminNavbar from "~/components/superadminlayout/navbar/AdminNavbar";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className="md:grid-cols-[252px_1fr]x lg:grid-cols-[252px_1fr]x grid min-h-[100dvh] grid-rows-[auto_1fr] overflow-hidden">
+      <div className="hidden border-r bg-muted/40 md:block"></div>
+      <div className="flex w-full flex-col gap-y-4 px-2 lg:gap-y-8 lg:px-4">
+        <AdminNavbar />
+        <div className="relative w-full">{children}</div>
+      </div>
+    </div>
+  );
 }
