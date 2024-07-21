6beb1aea7d72053e3359b4ec6a4a26f1ee956384
Eze Onyemaechi <105824065+iamExony@users.noreply.github.com>
2024-07-21 01:54:36 +0100

    feat: implement login with magic link

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -240,7 +240,8 @@ const StyleGuide: React.FC = () => {
               <br />
               icon<span className="text-pink-400">?: </span>React.ReactNode;
               <br />
-              children<span className="text-pink-400">?: </span>React.ReactNode;
+              children<span className="text-pink-400">?: </span>
+              React.ReactNode;
               <br />
               isLoading<span className="text-pink-400">?: </span>boolean;
               <br />
