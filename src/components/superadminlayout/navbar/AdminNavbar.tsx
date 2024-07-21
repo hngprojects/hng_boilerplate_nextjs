da6d299f9a9b0dd7b790da6a3a21b482035d114b
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2024-07-21 17:30:01 +0100

    feat: onclick pop up visible

--- src/components/superadminlayout/navbar/AdminNavbar.tsx
+++ src/components/superadminlayout/navbar/AdminNavbar.tsx
@@ -1,5 +1,11 @@
 import { BellIcon, ChevronDown, HelpCircle, SearchIcon } from "lucide-react";
 
+import UnreadNotificationCard from "~/components/common/UnreadNotificationCard/UnreadNotificationCard";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 import { Avatar, AvatarFallback, AvatarImage } from "../../ui/avatar";
 
 const AdminNavbar = () => {
@@ -8,26 +14,43 @@ const AdminNavbar = () => {
       <div className="flex h-10 items-center justify-between gap-2 rounded-[6px] border border-[#E4E4E7] px-3 text-sm font-normal placeholder:text-sm">
         <SearchIcon
           data-testid="search"
-          className="text-neutral-dark-2 h-4 w-4"
+          className="h-4 w-4 text-neutral-dark-2"
         />
         <input
-          className="text-neutral-dark-2 placeholder:text-neutral-dark-1 h-full w-full border-none outline-none ring-0"
+          className="h-full w-full border-none text-neutral-dark-2 outline-none ring-0 placeholder:text-neutral-dark-1"
           placeholder="Search option..."
           data-testid="input"
         />
       </div>
       <div className="flex w-full max-w-[152px] items-center justify-between gap-1">
         <div className="relative">
-          <BellIcon
-            data-testid="bell"
-            className="text-neutral-dark-2 hover:text-neutral-dark-1 h-6 w-6 transition-colors duration-300 hover:cursor-pointer"
-          />
-          <span className="bg-error absolute right-1 top-0 h-[6px] w-[6px] rounded-full"></span>
+          <Popover>
+            <PopoverTrigger>
+              <BellIcon
+                data-testid="bell"
+                className="h-6 w-6 text-neutral-dark-2 transition-colors duration-300 hover:cursor-pointer hover:text-neutral-dark-1"
+              />
+            </PopoverTrigger>
+            <PopoverContent
+              align="end"
+              className="w-[380px] border-none p-0 shadow-none"
+            >
+              <UnreadNotificationCard
+                notificationsPreview={[
+                  { header: "Check mail", time: "1 hour ago" },
+                  { header: "Sign up for offer", time: "2 hours ago" },
+                  { header: "Register for event", time: "1 hour ago" },
+                ]}
+                unreadCount={30}
+              />
+            </PopoverContent>
+          </Popover>
+          <span className="absolute right-1 top-0 h-[6px] w-[6px] rounded-full bg-error"></span>
         </div>
         <div>
           <HelpCircle
             data-testid="help"
-            className="text-neutral-dark-2 hover:text-neutral-dark-1 h-6 w-6 transition-colors duration-300 hover:cursor-pointer"
+            className="h-6 w-6 text-neutral-dark-2 transition-colors duration-300 hover:cursor-pointer hover:text-neutral-dark-1"
           />
         </div>
         <div className="hover:bg-black-1 flex w-full max-w-[64px] cursor-pointer items-center justify-between gap-2">
@@ -37,7 +60,7 @@ const AdminNavbar = () => {
           </Avatar>
           <ChevronDown
             data-testid="chevronDown"
-            className="2-5 text-neutral-dark-1 h-5"
+            className="2-5 h-5 text-neutral-dark-1"
           />
         </div>
       </div>
