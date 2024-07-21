16afd354a1960c34ac05bf1bf2a8ca441c2cdb8b
theblvckdev <nathancodes05@gmail.com>
2024-07-21 20:19:03 +0100

    feat: initial commit

---
+++ src/app/blog/latest/page.tsx
@@ -0,0 +1,22 @@
+const LatestArticlesPage = () => {
+  return (
+    <>
+      <main className="mx-auto max-w-6xl px-5 py-5 md:px-10 md:py-10 lg:py-20">
+        <h1 className="text-[28px] font-[700] leading-[33.89px] text-neutral-dark-1 md:text-[30px] md:leading-[43.57px] lg:text-center lg:text-[36px]">
+          Latest Articles
+        </h1>
+        <div className="mt-2">
+          {/* {articlesData.map((data, index) => {
+            const { author, avatar, content, datePublished, minsRead, tag, thumbnail, title } = data;
+
+            return (
+
+            )
+          })} */}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default LatestArticlesPage;
