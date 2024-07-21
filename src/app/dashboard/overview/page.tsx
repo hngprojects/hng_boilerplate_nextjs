9d058ecfe227f2adb02865f9791dff1b621efb84
kaleabendrias <kaleabendrias1212@gmail.com>
2024-07-21 03:14:15 +0300

    chore: Add Sidebar component to dashboard overview page
    Refactor the `src/app/dashboard/overview/page.tsx` file to include a new `Sidebar` component alongside the existing `Navbar` component. This change enhances the layout of the dashboard overview page and improves the user experience.

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "~/components/layouts/Navbar";
+import Sidebar from "~/components/layouts/Sidebar";
 import Overview from "../../../components/overview/overview";
 
 const page = () => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       <Overview />
     </div>
   );
