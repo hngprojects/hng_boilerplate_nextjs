2baad40d4b07600ea4f22050c413064082088a20
kaleabendrias <kaleabendrias1212@gmail.com>
2024-07-21 03:05:04 +0300

    chore: Add overview page component
    This commit adds a new component called "page" to the `src/app/dashboard/overview` directory. The component includes a navbar and an overview section. This change enhances the dashboard's functionality and improves the user experience.

---
+++ src/app/dashboard/overview/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "~/components/layouts/Navbar";
+import Overview from "../../../components/overview/overview";
+
+const page = () => {
+  return (
+    <div>
+      <Navbar />
+      <Overview />
+    </div>
+  );
+};
+
+export default page;
