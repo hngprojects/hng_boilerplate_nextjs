54170a848587efbf906af6ab875a35e4157f6528
= <=>
2024-07-21 17:11:53 +0100

    feat: update PricePlanCard test file

--- src/components/common/PricePlanCard/PricePlanCard.test.tsx
+++ src/components/common/PricePlanCard/PricePlanCard.test.tsx
@@ -1,4 +1,3 @@
-// components/PricePlanCard.test.tsx
 import { render, screen } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
@@ -25,10 +24,26 @@ vi.mock("./PricePlanBenefit", () => ({
 }));
 
 describe("pricePlanCard", () => {
-  it("renders the PricePlanCard component with correct props", () => {
+  const defaultBenefits = [
+    { benefitTitle: "2 Projects", isAddedBenefit: true },
+    { benefitTitle: "Up to 100 subscribers", isAddedBenefit: true },
+    { benefitTitle: "Basic analytics", isAddedBenefit: true },
+    { benefitTitle: "24-hour support response time", isAddedBenefit: true },
+    { benefitTitle: "Marketing advisor", isAddedBenefit: false },
+    { benefitTitle: "Custom integration", isAddedBenefit: false },
+  ];
+
+  it("renders the PricePlanCard component with default props", () => {
     expect.assertions(11); // We have 11 assertions in this test
 
-    render(<PricePlanCard type="Pro" price="$50" isActive={true} />);
+    render(
+      <PricePlanCard
+        type="Pro"
+        price="$50"
+        isActive={true}
+        lists={defaultBenefits}
+      />,
+    );
 
     // Check for the type
     expect(screen.getByText("Pro")).toBeInTheDocument();
@@ -45,14 +60,53 @@ describe("pricePlanCard", () => {
     ).toBeInTheDocument();
 
     // Check for the benefits
-    expect(screen.getByText("2 Projects")).toBeInTheDocument();
-    expect(screen.getByText("Up to 100 subscribers")).toBeInTheDocument();
-    expect(screen.getByText("Basic analytics")).toBeInTheDocument();
+    for (const benefit of defaultBenefits) {
+      expect(screen.getByText(benefit.benefitTitle)).toBeInTheDocument();
+    }
+
+    // Check for the button
+    expect(screen.getByText("Continue")).toBeInTheDocument();
+  });
+
+  it("renders the PricePlanCard component with different props", () => {
+    expect.assertions(11); // We have 11 assertions in this test
+
+    const newBenefits = [
+      { benefitTitle: "10 Projects", isAddedBenefit: true },
+      { benefitTitle: "Up to 1000 subscribers", isAddedBenefit: true },
+      { benefitTitle: "Advanced analytics", isAddedBenefit: true },
+      { benefitTitle: "12-hour support response time", isAddedBenefit: true },
+      { benefitTitle: "Personalized marketing advisor", isAddedBenefit: true },
+      { benefitTitle: "Custom API integration", isAddedBenefit: true },
+    ];
+
+    render(
+      <PricePlanCard
+        type="Enterprise"
+        price="$200"
+        isActive={false}
+        lists={newBenefits}
+      />,
+    );
+
+    // Check for the type
+    expect(screen.getByText("Enterprise")).toBeInTheDocument();
+
+    // Check for the price
+    expect(screen.getByText("$200")).toBeInTheDocument();
+    expect(screen.getByText("/month")).toBeInTheDocument();
+
+    // Check for the description
     expect(
-      screen.getByText("24-hour support response time"),
+      screen.getByText(
+        "The essensitals to provide your best work for clients.",
+      ),
     ).toBeInTheDocument();
-    expect(screen.getByText("Marketing advisor")).toBeInTheDocument();
-    expect(screen.getByText("Custom integration")).toBeInTheDocument();
+
+    // Check for the benefits
+    for (const benefit of newBenefits) {
+      expect(screen.getByText(benefit.benefitTitle)).toBeInTheDocument();
+    }
 
     // Check for the button
     expect(screen.getByText("Continue")).toBeInTheDocument();
@@ -62,12 +116,22 @@ describe("pricePlanCard", () => {
     expect.assertions(2); // We have 2 assertions in this test
 
     const { container: activeContainer } = render(
-      <PricePlanCard type="Pro" price="$50" isActive={true} />,
+      <PricePlanCard
+        type="Pro"
+        price="$50"
+        isActive={true}
+        lists={defaultBenefits}
+      />,
     );
     expect(activeContainer.firstChild).toHaveClass("border-primary");
 
     const { container: inactiveContainer } = render(
-      <PricePlanCard type="Pro" price="$50" isActive={false} />,
+      <PricePlanCard
+        type="Pro"
+        price="$50"
+        isActive={false}
+        lists={defaultBenefits}
+      />,
     );
     expect(inactiveContainer.firstChild).toHaveClass("border-border");
   });
