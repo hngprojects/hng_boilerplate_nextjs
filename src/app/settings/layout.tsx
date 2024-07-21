b73c35e41aa750f4a601c4d2b3a18618115c26e8
oreoluwa212 <oreoluwaajayyiruth@gmail.com>
2024-07-21 20:47:05 +0100

    Revert "Revert "Feat/10 profile settings page""

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,4 +1,6 @@
 import SettingsSidebar from "~/components/common/settings/sidebar";
+import { UpdatedModal } from "./profile";
+import StateContextProvider from "./stateContext";
 
 export default function SettingsLayout({
   children,
@@ -6,14 +8,13 @@ export default function SettingsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <StateContextProvider>
       <header>Navbar</header>
       <SettingsSidebar />
-      <section className="relative w-full overflow-x-hidden md:pl-[126px] min-[1140px]:pl-[290px]">
-        <div className="relative flex h-full w-full flex-col pt-12 md:pt-0">
-          {children}
-        </div>
+      <section className="relative w-full overflow-x-hidden bg-white md:pl-[126px] min-[1140px]:pl-[290px]">
+        <div className="relative flex h-full w-full flex-col">{children}</div>
+        <UpdatedModal />
       </section>
-    </>
+    </StateContextProvider>
   );
 }
