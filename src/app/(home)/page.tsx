f088a64f5fcc2e42f510eb88146642f344f6ff9d
Mary <101870828+Chioma227@users.noreply.github.com>
2024-07-21 14:16:34 +0100

    fix: fixed merge conflict

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,17 @@
-import BlogCard from "~/components/blogCard/BlogCard";
+import Hero from "~/components/layouts/homepage/Hero";
+import HowItWorks from "~/components/layouts/homepage/HowItWorks";
+import PerfectFit from "~/components/layouts/homepage/PerfectFit";
+import Testimonials from "~/components/layouts/homepage/Testimonials";
+import UserSection from "~/components/layouts/homepage/UserSection";
 
 export default function Home() {
   return (
     <>
-      <BlogCard />
-      {/* <Hero />
+      <Hero />
       <UserSection />
       <HowItWorks />
       <Testimonials />
-      <PerfectFit /> */}
+      <PerfectFit />
     </>
   );
 }
