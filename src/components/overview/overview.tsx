1e8c47027b7c00acc460ffc2c97e3c35336dcd1a
kaleabendrias <kaleabendrias1212@gmail.com>
2024-07-21 03:14:15 +0300

    chore: Add Sidebar component to dashboard overview page
    Refactor the `src/app/dashboard/overview/page.tsx` file to include a new `Sidebar` component alongside the existing `Navbar` component. This change enhances the layout of the dashboard overview page and improves the user experience.

--- src/components/overview/overview.tsx
+++ src/components/overview/overview.tsx
@@ -75,7 +75,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="p-4 md:p-8">
+    <div className="mx-12 p-4 md:p-8">
       <div className="mb-6 flex flex-col items-center justify-between md:flex-row">
         <div className="mb-4 md:mb-0">
           <h1 className="text-2xl font-bold md:text-3xl">Dashboard</h1>
