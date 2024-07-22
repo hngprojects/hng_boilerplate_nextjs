caa654403ea9fa3edc67066ab3af7dee6b6397fb
omotayobolu <omotayodaniel10@gmail.com>
2024-07-22 08:39:30 +0100

    fix: Removed import of describe, expect and it

--- src/app/signup/page.test.tsx
+++ src/app/signup/page.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect, it, vi } from "vitest";
 
 import SignUp from "./page";
 
