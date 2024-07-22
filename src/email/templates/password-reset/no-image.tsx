e04fd782cb49f9618592451f1ab6c3d0cf4eed60
Obi Fortune <gabrielobi.of@gmail.com>
2024-07-21 20:09:57 -0400

    fix the positon of the tailwind wrapper

--- src/email/templates/password-reset/no-image.tsx
+++ src/email/templates/password-reset/no-image.tsx
@@ -1,6 +1,14 @@
-import { Button, Section, Text } from "@react-email/components";
+import {
+  Button,
+  Head,
+  Html,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
 
 import EmailResetPasswordLayout from "~/email/templates/_components/emailLayouts/emailResetPasswordLayout";
+import TailwindWrapper from "../_components/tailwindWrapper";
 
 interface IProperties {
   name: string;
@@ -11,33 +19,37 @@ export default function ResetPasswordEmail(properties: IProperties) {
   const { name, resetLink } = properties;
 
   return (
-    <EmailResetPasswordLayout>
-      <Text className="text-center text-2xl font-bold">
-        Reset Your Password
-      </Text>
-      <Text className="mb-4">Hi {name},</Text>
-      <Text className="mb-4">
-        You recently requested to reset your password. If you did not make this
-        request, you can ignore this email.
-      </Text>
-      <Text className="mt-4">
-        To reset your password, please click the button below.
-      </Text>
-      <Section className="text-center">
-        <Button
-          href={resetLink}
-          className="w-56 rounded bg-[#F97316] px-4 py-2 font-medium text-background text-white"
-        >
-          Reset Password
-        </Button>
-      </Section>
+    <TailwindWrapper>
+      <Html>
+        <Head />
+        <Preview>{`Password Reset for ${name}`}</Preview>
+        <EmailResetPasswordLayout>
+          <Text className="text-center text-2xl font-bold">
+            Reset Your Password
+          </Text>
+          <Text className="mb-4">Hi {name},</Text>
+          <Text className="mb-4">
+            You recently requested to reset your password. If you did not make
+            this request, you can ignore this email.
+          </Text>
+          <Text className="mt-4">
+            To reset your password, please click the button below.
+          </Text>
+          <Section className="text-center">
+            <Button
+              href={resetLink}
+              className="w-56 rounded bg-[#F97316] px-4 py-2 font-medium text-background text-white"
+            />
+          </Section>
 
-      <Text className="mb-6 mt-8">
-        Regards,
-        <br />
-        Boilerplate Team
-      </Text>
-    </EmailResetPasswordLayout>
+          <Text className="mb-6 mt-8">
+            Regards,
+            <br />
+            Boilerplate Team
+          </Text>
+        </EmailResetPasswordLayout>
+      </Html>
+    </TailwindWrapper>
   );
 }
 
