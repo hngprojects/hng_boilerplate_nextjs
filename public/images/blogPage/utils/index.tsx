4764660aa212291bb79754546e78580eb109567e
Adeyinka Akinsanya <akinsanyaadeyinka4166@gmail.com>
2024-07-22 19:02:59 +0100

    Update public/images/blogPage/utils/index.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Signed-off-by: Adeyinka Akinsanya <akinsanyaadeyinka4166@gmail.com>

--- public/images/blogPage/utils/index.tsx
+++ public/images/blogPage/utils/index.tsx
@@ -6,4 +6,12 @@ import blogCard5 from "../blogCard5.png";
 import blogCard6 from "../blogCard6.png";
 import blogHero from "../blogHero.png";
 
-export { blogHero, blogCard1, blogCard2, blogCard3, blogCard4, blogCard5, blogCard6 }
+export {
+  blogHero,
+  blogCard1,
+  blogCard2,
+  blogCard3,
+  blogCard4,
+  blogCard5,
+  blogCard6,
+};
