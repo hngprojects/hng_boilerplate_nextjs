050dc8042547935f723b477285b40455adca98bb
Aghogho Emokiniovo <emokiniovoaghogho@gmail.com>
2024-07-22 16:25:01 +0100

    Update NotificationSettings.test.jsx
    added correct file path for notification settings file

--- src/test/NotificationSettings.test.jsx
+++ src/test/NotificationSettings.test.jsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
-import NotificationSettings from "../app/settings/notification-settings/page";
+import NotificationSettings from "../app/settings/notification-settings/notificationsettings";
 
 const SETTINGS_DATA = [
   {
