d2549813bfc2207917aba47eb31e1ca148a2738f
Adeyinka Akinsanya <akinsanyaadeyinka4166@gmail.com>
2024-07-22 19:02:48 +0100

    Update public/images/blogPage/utils/index.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Signed-off-by: Adeyinka Akinsanya <akinsanyaadeyinka4166@gmail.com>

--- public/images/blogPage/utils/index.tsx
+++ public/images/blogPage/utils/index.tsx
@@ -4,7 +4,6 @@ import blogCard3 from "../blogCard3.png";
 import blogCard4 from "../blogCard4.png";
 import blogCard5 from "../blogCard5.png";
 import blogCard6 from "../blogCard6.png";
-import blogCard3 from "../blogCard3.png";
 import blogHero from "../blogHero.png";
 
 export { blogHero, blogCard1, blogCard2, blogCard3, blogCard4, blogCard5, blogCard6 }
