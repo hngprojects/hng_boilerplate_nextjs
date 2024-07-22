5024b048b3bca2f8cad67df9de0f934c70bfc7d7
oree <oreoluwaajayyiruth@gmail.com>
2024-07-22 11:56:00 +0100

    feat: blog landing page

---
+++ public/images/blogPage/utils/index.tsx
@@ -0,0 +1,9 @@
+import blogCard1 from "../blogCard1.png";
+import blogCard2 from "../blogCard2.png";
+import blogCard4 from "../blogCard4.png";
+import blogCard5 from "../blogCard5.png";
+import blogCard6 from "../blogCard6.png";
+import blogCard3 from "../blogCard3.png";
+import blogHero from "../blogHero.png";
+
+export { blogHero, blogCard1, blogCard2, blogCard3, blogCard4, blogCard5, blogCard6 }
