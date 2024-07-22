a937054f3ed35c3d4bc582150b6539e063a086e1
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-07-22 10:18:17 +0100

    feat: pass test

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,8 +4,6 @@ import AdminLayout from "~/components/superadminlayout";
 
 interface IProperties {
   children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
 }
 
 const layout: React.FC<IProperties> = ({ children }) => {
