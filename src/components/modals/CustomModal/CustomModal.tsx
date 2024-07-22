73277ca911f4ad64d57969f8f6150785f1b7b38f
= <=>
2024-07-22 22:43:08 +0100

    fix(HNG-5): fix package issues for custommodal

--- src/components/modals/CustomModal/CustomModal.tsx
+++ src/components/modals/CustomModal/CustomModal.tsx
@@ -1,7 +1,23 @@
-import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, ReactNode } from "react";
+import { ReactNode } from "react";
 
 import CustomButton from "~/components/common/Button/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+} from "~/components/ui/dialog";
+
+type Variant =
+  | "default"
+  | "primary"
+  | "destructive"
+  | "subtle"
+  | "loading"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | "link";
 
 interface CustomModalProperties {
   onOpen: boolean;
@@ -9,7 +25,7 @@ interface CustomModalProperties {
   buttonAction: () => void;
   showButtons: boolean;
   actionButtonText: string;
-  actionButtonColor: string;
+  actionButtonColor?: Variant;
   children: ReactNode;
   buttonPosition: string;
   addCloseButton: boolean;
@@ -27,64 +43,28 @@ const CustomModal: React.FC<CustomModalProperties> = ({
   addCloseButton,
 }) => {
   return (
-    <Transition.Root show={onOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-[1000]" onClose={toggleState}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto">
-          <div className="flex min-h-full min-w-[400px] max-w-[800px] items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative flex max-h-[500px] min-h-[200px] w-full transform flex-col overflow-hidden rounded-lg bg-white p-[24px] text-left shadow-xl transition-all sm:my-8">
-                <div className="w-full flex-[5]">{children}</div>
-                {showButtons && (
-                  <div
-                    className={`flex w-full flex-[1] ${buttonPosition == "right" ? "justify-end" : buttonPosition == "left" ? "justify-start" : "justify-center"}`}
-                  >
-                    <div className="flex flex-row gap-[8px]">
-                      {addCloseButton && (
-                        <CustomButton
-                          variant="outline"
-                          isDisabled={false}
-                          onClick={() => toggleState(!onOpen)}
-                        >
-                          Close
-                        </CustomButton>
-                      )}
-
-                      <CustomButton
-                        variant={actionButtonColor}
-                        isDisabled={false}
-                        onClick={buttonAction}
-                      >
-                        {actionButtonText}
-                      </CustomButton>
-                    </div>
-                  </div>
-                )}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+    <Dialog open={onOpen} onOpenChange={toggleState}>
+      <DialogContent className="sm:max-w-md" aria-describedby="modal-content">
+        <div className="w-full flex-[5]">{children}</div>
+        {showButtons && (
+          <DialogFooter
+            data-testid="dialog-footer"
+            className={`flex ${buttonPosition === "right" ? "justify-end" : buttonPosition === "left" ? "justify-start" : "justify-center"}`}
+          >
+            <div className="flex flex-row gap-2">
+              {addCloseButton && (
+                <DialogClose asChild>
+                  <CustomButton variant="outline">Close</CustomButton>
+                </DialogClose>
+              )}
+              <CustomButton variant={actionButtonColor} onClick={buttonAction}>
+                {actionButtonText}
+              </CustomButton>
+            </div>
+          </DialogFooter>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 };
 
---
+++ src/test/modals/CustomModal.test.tsx
@@ -0,0 +1,233 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it, vi } from "vitest";
+
+import CustomModal from "~/components/modals/CustomModal/CustomModal";
+
+describe("customModal Component", () => {
+  it("renders the modal when onOpen is true", () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={true}
+      />,
+    );
+    expect(screen.getByRole("dialog")).toBeInTheDocument();
+  });
+
+  it("does not render the modal when onOpen is false", () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={false}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={true}
+      />,
+    );
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+
+  it("calls toggleState with false when close button is clicked", async () => {
+    expect.hasAssertions();
+    const toggleStateMock = vi.fn();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={toggleStateMock}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={true}
+      />,
+    );
+    const closeButtons = screen.getAllByRole("button", { name: /close/i });
+    await userEvent.click(closeButtons[0]);
+    expect(toggleStateMock).toHaveBeenCalledWith(false);
+  });
+
+  it("calls buttonAction when action button is clicked", async () => {
+    expect.hasAssertions();
+    const buttonActionMock = vi.fn();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={buttonActionMock}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={true}
+      />,
+    );
+    const actionButton = screen.getByRole("button", { name: /delete/i });
+    await userEvent.click(actionButton);
+    expect(buttonActionMock).toHaveBeenCalled();
+  });
+
+  it("does not show buttons if showButtons is false", () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={false}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={false}
+      />,
+    );
+    expect(screen.queryByRole("button", { name: /delete item/i })).toBeNull();
+    expect(screen.queryByRole("button", { name: /close window/i })).toBeNull();
+  });
+
+  it("shows close button if addCloseButton is true", () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={true}
+      />,
+    );
+    const closeButtons = screen.getAllByRole("button", { name: /close/i });
+    expect(closeButtons.length).toBeGreaterThan(0);
+  });
+
+  it("does not show close button if addCloseButton is false", () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={false}
+      />,
+    );
+    expect(screen.queryByRole("button", { name: /close window/i })).toBeNull();
+  });
+
+  it("positions buttons correctly based on buttonPosition prop", () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="left"
+        addCloseButton={true}
+      />,
+    );
+
+    const dialogFooter = screen.getByTestId("dialog-footer");
+
+    expect(dialogFooter).toHaveClass("justify-start");
+  });
+
+  it("renders internal content correctly", () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={true}
+      />,
+    );
+    expect(screen.getByText(/modal content/i)).toBeInTheDocument();
+  });
+
+  it("supports keyboard navigation and screen reader compatibility", async () => {
+    expect.hasAssertions();
+    render(
+      <CustomModal
+        onOpen={true}
+        toggleState={vi.fn()}
+        buttonAction={vi.fn()}
+        showButtons={true}
+        actionButtonText="Delete"
+        actionButtonColor="primary"
+        // eslint-disable-next-line react/no-children-prop
+        children={<p>Modal Content</p>}
+        buttonPosition="right"
+        addCloseButton={true}
+      />,
+    );
+    const closeButton = screen.getAllByRole("button", { name: /close/i })[0];
+    closeButton.focus();
+    expect(closeButton).toHaveFocus();
+    await userEvent.keyboard("[Enter]");
+  });
+
+  it("renders consistently across different browsers", () => {
+    expect.hasAssertions();
+    const mockRender = vi.fn(() =>
+      render(
+        <CustomModal
+          onOpen={true}
+          toggleState={vi.fn()}
+          buttonAction={vi.fn()}
+          showButtons={true}
+          actionButtonText="Delete"
+          actionButtonColor="primary"
+          // eslint-disable-next-line react/no-children-prop
+          children={<p>Modal Content</p>}
+          buttonPosition="right"
+          addCloseButton={true}
+        />,
+      ),
+    );
+    mockRender();
+    expect(mockRender).toHaveBeenCalledTimes(1);
+  });
+});
