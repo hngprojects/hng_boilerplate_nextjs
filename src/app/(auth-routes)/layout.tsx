4dfe4f5abcc1b32e7f97d5dc283b41840d798d76
Tania <38540885+crystal4000@users.noreply.github.com>
2024-07-23 18:02:02 -0500

    Completed Login Pages

--- src/app/(auth-routes)/layout.tsx
+++ src/app/(auth-routes)/layout.tsx
@@ -1,12 +1,11 @@
-import Footer from "../../components/layouts/footer/index.tsx";
-import Navbar from "../../components/layouts/navbar/index.tsx";
+import Navbar from "~/components/layouts/navbar";
 
-function Layout({ children }) {
+function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       <Navbar />
       <div className="mx-auto w-5/6">{children}</div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/app/(auth-routes)/login/page.tsx
+++ src/app/(auth-routes)/login/page.tsx
@@ -1,7 +1,221 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff, ShieldCheck } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "~/components/ui/button";
+import { Checkbox } from "~/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+const loginSchema = z.object({
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+  rememberMe: z.boolean().default(false),
+});
+
+type LoginFormData = z.infer<typeof loginSchema>;
+
+const getInputClassName = (hasError: boolean, isValid: boolean) => {
+  const baseClasses =
+    "font-inter w-full rounded-md border px-3 py-3 text-sm font-normal leading-[21.78px] transition duration-150 ease-in-out focus:outline-none focus:ring-1 focus:ring-opacity-50";
+
+  if (hasError) {
+    return `${baseClasses} border-red-500 focus:border-red-500 focus:ring-red-500 text-red-900`;
+  } else if (isValid) {
+    return `${baseClasses} border-orange-500 focus:border-orange-500 focus:ring-orange-500 text-neutralColor-dark-2`;
+  }
+  return `${baseClasses} border-gray-300 focus:border-orange-500 focus:ring-orange-500 text-neutralColor-dark-2`;
+};
+
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const form = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+  });
+
+  const onSubmit = (data: LoginFormData) => {
+    console.log("Form submitted:", data);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      <h1>login</h1>
+    <div className="flex min-h-full items-center justify-center px-4 py-10 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div className="text-center">
+          <h1 className="font-inter text-neutralColor-dark-2 mb-5 text-center text-2xl font-semibold leading-tight">
+            Login
+          </h1>
+          <p className="font-inter text-neutralColor-dark-2 mt-2 text-center text-sm font-normal leading-6">
+            Welcome back, you&apos;ve been missed!
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <hr className="w-full border-t border-gray-300" />
+          <span className="font-inter text-neutralColor-dark-1 px-3 text-xs font-normal leading-tight">
+            OR
+          </span>
+          <hr className="w-full border-t border-gray-300" />
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-neutralColor-dark-2 sr-only">
+                    Email
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter Email Address"
+                      {...field}
+                      className={getInputClassName(
+                        !!form.formState.errors.email,
+                        form.formState.isSubmitted &&
+                          !form.formState.errors.email,
+                      )}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only">Password</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter Password"
+                        {...field}
+                        className={getInputClassName(
+                          !!form.formState.errors.password,
+                          form.formState.isSubmitted &&
+                            !form.formState.errors.password,
+                        )}
+                      />
+                      <button
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className="absolute inset-y-0 right-0 flex items-center pr-3"
+                      >
+                        {showPassword ? (
+                          <Eye className="h-5 w-5 text-gray-400" />
+                        ) : (
+                          <EyeOff className="h-5 w-5 text-gray-400" />
+                        )}
+                      </button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between">
+              <FormField
+                control={form.control}
+                name="rememberMe"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormLabel>Remember me</FormLabel>
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="text-neutralColor-dark-2 text-sm font-medium"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+            </div>
+            <Button
+              type="submit"
+              variant="default"
+              size="default"
+              className="h-12 w-full rounded-md bg-primary px-4 py-3 text-white hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+            >
+              Login
+            </Button>
+          </form>
+        </Form>
+
+        <Button
+          type="button"
+          variant="outline"
+          size="default"
+          className="text-neutralColor-dark-2 hover:bg-gray50 w-full rounded-md border border-stroke-colors-stroke bg-white px-4 py-3 text-sm font-medium focus:outline-none"
+        >
+          <Link href="/login/magic-link">Sign in with magic link</Link>
+        </Button>
+
+        <p className="font-inter text-neutralColor-dark-1 mt-5 text-center text-sm font-normal leading-[15.6px]">
+          Don&apos;t Have An Account?{" "}
+          <a
+            href="#"
+            className="font-inter ms-1 text-left text-base font-bold leading-[19.2px] text-primary hover:text-orange-400"
+          >
+            Sign Up
+          </a>
+        </p>
+
+        <p className="mt-2 text-center text-xs text-gray-500">
+          <ShieldCheck className="mr-1 hidden h-4 w-4 text-gray-400 sm:inline-block" />
+          By logging in, you agree to the{" "}
+          <a
+            href="#"
+            className="text-sm font-bold text-primary hover:text-orange-500"
+          >
+            Terms of Service
+          </a>{" "}
+          and{" "}
+          <a
+            href="#"
+            className="text-sm font-bold text-primary hover:text-orange-500"
+          >
+            Privacy Policy
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
