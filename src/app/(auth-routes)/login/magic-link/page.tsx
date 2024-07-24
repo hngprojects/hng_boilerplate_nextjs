37163871c9e73586529de6621fbad504fb561ddf
Tania <38540885+crystal4000@users.noreply.github.com>
2024-07-24 13:26:54 -0500

    Fixed Design errors

--- src/app/(auth-routes)/login/magic-link/page.tsx
+++ src/app/(auth-routes)/login/magic-link/page.tsx
@@ -62,7 +62,7 @@ const LoginMagicLink: React.FC = () => {
   };
 
   return (
-    <div className="mt-16 flex min-h-full items-center justify-center bg-gray-50 px-0 sm:px-6 lg:px-8">
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-0 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div className="text-center">
           <h1 className="font-inter text-neutralColor-dark-2 mb-5 text-center text-2xl font-semibold leading-tight">
