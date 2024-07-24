17c70a171388eb3b41ecdb0f5f4dfed2db9e065f
Vxrcel <skyeparagon@gmail.com>
2024-07-24 14:31:11 +0100

    Fix: admin dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,20 @@
-const layout = () => {
-  return <div>layout</div>;
-};
+import { Suspense } from "react";
 
-export default layout;
+// import AdminNavbar from "~/components/superadminlayout/navbar/AdminNavbar";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="md:grid-cols-[252px_1fr]x lg:grid-cols-[252px_1fr]x grid min-h-[100dvh] grid-rows-[auto_1fr]">
+      {/* <div className="hidden border-r bg-muted/40 md:block" /> */}
+      {/* <AdminNavbar /> */}
+
+      <div className="relative w-full bg-white px-2 max-lg:overflow-hidden xl:px-4">
+        <Suspense>{children}</Suspense>
+      </div>
+    </div>
+  );
+}
