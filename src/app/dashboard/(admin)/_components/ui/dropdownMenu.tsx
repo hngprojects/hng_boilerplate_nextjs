571296de56aafd22c0c987d64e66f7da2b03e979
theblvckdev <nathancodes05@gmail.com>
2024-07-25 10:11:05 +0100

    created dropdown component for admin dashboard

--- src/app/dashboard/(admin)/_components/ui/dropdownMenu.tsx
+++ src/app/dashboard/(admin)/_components/ui/dropdownMenu.tsx
@@ -1,9 +1,21 @@
-const dropdownMenu = () => {
+const DropdownMenu = ({
+  width,
+  active,
+  children,
+}: {
+  width: string;
+  active: boolean;
+  children: React.ReactNode;
+}) => {
   return (
     <>
-      <div className="absolute"></div>
+      <div
+        className={`absolute bottom-0 right-0 top-full mt-2 h-fit overflow-hidden rounded-md bg-white p-2 shadow ring-1 ring-border duration-200 ease-in ${width} ${active ? "visible translate-y-0 opacity-100" : "invisible -translate-y-[10px] opacity-0"}`}
+      >
+        {children}
+      </div>
     </>
   );
 };
 
-export default dropdownMenu;
+export default DropdownMenu;
--- src/app/dashboard/(admin)/admin/users/page.tsx
+++ src/app/dashboard/(admin)/admin/users/page.tsx
@@ -1,10 +1,56 @@
-import { CirclePlus } from "lucide-react";
+"use client";
+
+import { Check, CirclePlus, Filter } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 import CardComponent from "~/components/adminDashboard/CardComponent";
 import CustomButton from "~/components/common/common-button/common-button";
+import DropdownMenu from "../../_components/ui/dropdownMenu";
 import { userCardData } from "./data/user-dummy-data";
 
+interface FilterDataProperties {
+  title: string;
+  selected: boolean;
+}
+
+const filterData: FilterDataProperties[] = [
+  {
+    title: "Active",
+    selected: false,
+  },
+
+  {
+    title: "Inactive",
+    selected: false,
+  },
+];
+
 const UserPage = () => {
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const [selectedFilter, setSelectedFilter] = useState<
+    FilterDataProperties | undefined
+  >();
+
+  const menuReference = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handler = (event: MouseEvent) => {
+      if (
+        menuReference.current &&
+        !menuReference.current.contains(event.target as Node)
+      ) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    // Cleanup function
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, []);
+
   return (
     <>
       <section>
@@ -32,6 +78,47 @@ const UserPage = () => {
             </div>
 
             <div className="flex flex-row items-center gap-3">
+              <div className="relative" ref={menuReference}>
+                <CustomButton
+                  onClick={() => setShowDropdown(!showDropdown)}
+                  size="lg"
+                  className="p-3"
+                  variant="outline"
+                >
+                  <div className="flex flex-row items-center gap-2">
+                    <Filter size={16} color="#525252" />
+                    <div className="text-base font-normal leading-5">
+                      Filter
+                    </div>
+                  </div>
+                </CustomButton>
+
+                <DropdownMenu width="w-[150px]" active={showDropdown}>
+                  {filterData.map((data, index) => {
+                    const { selected, title } = data;
+
+                    selectedFilter?.title === title
+                      ? (data.selected = true)
+                      : (data.selected = false);
+
+                    return (
+                      <button
+                        onClick={() => setSelectedFilter(filterData[index])}
+                        key={index}
+                        className="flex w-full items-center gap-2 px-2 py-1.5 text-left text-base font-normal leading-4 text-foreground outline-none"
+                      >
+                        <div className="mr-auto">{title}</div>
+                        <Check
+                          size={16}
+                          color="#09090b"
+                          className={`${selected ? "opacity-100" : "opacity-0"}`}
+                        />
+                      </button>
+                    );
+                  })}
+                </DropdownMenu>
+              </div>
+
               <CustomButton size="lg" className="p-3" variant="primary">
                 <div className="flex flex-row items-center gap-2">
                   <CirclePlus size={16} color="#FFFFFF" />
