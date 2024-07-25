5241bc3c77f5ec78f1a6812a240d953cda371a5a
theblvckdev <nathancodes05@gmail.com>
2024-07-25 10:10:42 +0100

    code updates

---
+++ src/app/dashboard/(admin)/_components/ui/dropdownMenu.tsx
@@ -0,0 +1,9 @@
+const dropdownMenu = () => {
+  return (
+    <>
+      <div className="absolute"></div>
+    </>
+  );
+};
+
+export default dropdownMenu;
--- src/app/dashboard/(admin)/admin/users/page.tsx
+++ src/app/dashboard/(admin)/admin/users/page.tsx
@@ -1,12 +1,4 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { CirclePlus, Filter } from "lucide-react";
+import { CirclePlus } from "lucide-react";
 
 import CardComponent from "~/components/adminDashboard/CardComponent";
 import CustomButton from "~/components/common/common-button/common-button";
@@ -40,25 +32,6 @@ const UserPage = () => {
             </div>
 
             <div className="flex flex-row items-center gap-3">
-              <DropdownMenu>
-                <DropdownMenuTrigger className="rounded p-2 outline-none ring-1 ring-stroke-colors-stroke">
-                  <div className="flex flex-row items-center gap-2">
-                    <Filter size={20} color="#525252" />
-                    <div className="text-base font-normal leading-5 text-neutral-dark-1">
-                      Filter
-                    </div>
-                  </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="mt-2 rounded-lg bg-white px-4 py-2.5 shadow-md">
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>Profile</DropdownMenuItem>
-                  <DropdownMenuItem>Billing</DropdownMenuItem>
-                  <DropdownMenuItem>Team</DropdownMenuItem>
-                  <DropdownMenuItem>Subscription</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-
               <CustomButton size="lg" className="p-3" variant="primary">
                 <div className="flex flex-row items-center gap-2">
                   <CirclePlus size={16} color="#FFFFFF" />
