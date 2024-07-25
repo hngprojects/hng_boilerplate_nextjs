b9a8e6b930aaaf0297fb6da4bad479ffbea62e49
Kingsley Solomon <kinxly@gmail.com>
2024-07-26 00:41:01 +0100

    feat: completed Subscription cancellation template

--- src/email/templates/subscription-cancellation-confirmed/no-image.tsx
+++ src/email/templates/subscription-cancellation-confirmed/no-image.tsx
@@ -13,7 +13,6 @@ import Layout from "../_components/layout/layout";
 interface SubscriptionConfirmationProperties {
   title?: string;
   username?: string;
-  image?: string;
   expireTime?: string;
   star?: string;
   description?: string;
@@ -27,22 +26,17 @@ interface SubscriptionConfirmationProperties {
 export const SubscriptionConfirmation = ({
   title = "",
   username = "",
-  image = "",
   star = "",
   description = "",
   link = "",
-  expireTime = "",
   data,
 }: SubscriptionConfirmationProperties) => {
   return (
     <Layout>
       <Section className="py-[56px]">
-        <Section className="mx-auto flex items-center justify-center md:w-[178px]">
-          <Img src={image} alt="hello" className="h-[100%] w-[100%]" />
-        </Section>
         <Section className="mx-auto flex items-center justify-center"></Section>
 
-        <Container className="max-w-[680px] px-[48px] md:px-0">
+        <Container className="my-0 max-w-[680px] px-[48px] md:px-0">
           <Section className="my-[40px] flex flex-col items-center justify-center">
             <Heading
               as="h5"
@@ -61,12 +55,12 @@ export const SubscriptionConfirmation = ({
             </Section>
 
             <Section className="">
-              <ul className="list-none pl-0">
+              <ul className="m-0 list-none pl-0">
                 {data?.map((userData, index) => {
                   return (
                     <li
                       key={index}
-                      className="my-[1rem] flex items-start text-[14px] md:text-[16px]"
+                      className="mt-[1rem] flex items-start text-[14px] md:text-[16px]"
                     >
                       <div className="mr-2 flex-shrink-0">
                         <Img
@@ -90,22 +84,8 @@ export const SubscriptionConfirmation = ({
               </ul>
             </Section>
           </Section>
-        </Container>
-
-        <Section className="w-full bg-[#F97316] py-[39px] text-center text-[#FAFAFA]">
-          <Container className="max-w-[680px] px-[48px] md:px-0">
-            <Text className="my-0 text-[18px] md:text-[24px]">
-              The estimated expiry date
-            </Text>
-            <Heading as="h2" className="my-0 text-[28px] md:text-[44px]">
-              {expireTime}
-            </Heading>
-          </Container>
-        </Section>
-
-        <Section className="text-center">
-          <Container className="mb-[40px] max-w-[680px] px-[48px] md:px-0">
-            <Text className="text-left leading-[19px] text-[#525252] md:text-[16px]">
+          <Section className="text-center">
+            <Text className="my-0 text-left leading-[19px] text-[#525252] md:text-[16px]">
               If you have any questions or need further assistance, please don’t
               hesitate to reach out to our customer{" "}
               <Link className="text-[#F97316]" href="/support">
@@ -115,22 +95,24 @@ export const SubscriptionConfirmation = ({
               <Link className="text-[#F97316]" href="/">
                 boilerplate@gmail.com.
               </Link>{" "}
-              We look forward to providing you with an exceptional experience.
-              Thank you for choosing our product!
+              Thank you for being a part of our community. We hope to serve you
+              again in the future!
             </Text>
-          </Container>
-          <Section className="mt-[32px] w-[100%] text-center">
-            <Container className="mb-[40px] max-w-[680px] px-[48px] md:px-0">
-              <Button
-                target={"_blank"}
-                className="w-[100%] rounded-[8px] bg-[#F97316] py-[16px] text-[#FAFAFA] md:w-fit md:px-[2rem]"
-                href={link}
-              >
-                Learn More About us
-              </Button>
-            </Container>
+
+            <Section className="mt-[32px] w-[100%] text-center">
+              <Container className="mb-[40px] max-w-[680px]">
+                <Button
+                  target={"_blank"}
+                  className="w-[100%] rounded-[8px] bg-[#F97316] py-[16px] text-[#FAFAFA] md:w-fit md:px-[2rem]"
+                  href={link}
+                >
+                  Learn More About us
+                </Button>
+              </Container>
+            </Section>
           </Section>
-        </Section>
+        </Container>
+
         <Section className="mt-[40px]">
           <Container className="max-w-[680px] px-[48px] md:px-0">
             <Text className="font-[600] text-[#121212]">
@@ -146,33 +128,24 @@ export const SubscriptionConfirmation = ({
 };
 
 SubscriptionConfirmation.PreviewProps = {
-  title: "Subscription Confirmation",
+  title: "Subscription Cancellation Confirmation",
   username: "John Doe",
-  image: "https://imgur.com/3p3JubF.png",
   star: "https://i.imgur.com/bmprMwh.png",
   expireTime: "17th September, 2024",
   link: "",
   description:
-    "Your payment was processed successfully. Thank you for subscribing to our Bi-monthly feature! We’re excited to have you on board. You’ll receive a separate receipt via email. Below are the details of your subscription:",
+    "We regret to inform you that your subscription to Bi-monthly features has been cancelled. We appreciate your support and hope you’ve enjoyed our services during your subscription period.",
   data: [
     {
-      key: "Email Address",
-      value: "johndoe@gmail.com",
+      key: "Subscription ID",
+      value: "123456",
     },
     {
       key: "Subscription Plan",
       value: "Bi-monthly Feature",
     },
     {
-      key: "Cost",
-      value: "$20.89",
-    },
-    {
-      key: "Duration",
-      value: "2 Months",
-    },
-    {
-      key: "Subscription Date",
+      key: "Cancellation Date",
       value: "17th July, 2024",
     },
   ],
