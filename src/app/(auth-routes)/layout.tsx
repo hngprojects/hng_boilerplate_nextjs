a065d80820633ab7daa26769d605cb0c99ec3deb
Vxrcel <skyeparagon@gmail.com>
2024-07-25 10:33:21 +0100

    purge pnpm cache

--- src/app/(auth-routes)/layout.tsx
+++ src/app/(auth-routes)/layout.tsx
@@ -1,6 +1,9 @@
+import Navbar from "~/components/layouts/navbar";
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Navbar is_auth_path />
       <div className="mx-auto w-5/6">{children}</div>
     </>
   );
