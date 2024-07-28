579641977c0657da214fee7c1f6ff038e230af11
Destiny Saturday <destiny@destinedcodes.com>
2024-07-28 18:56:56 +0100

    Update team_deploy.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- scripts/team_deploy.sh
+++ scripts/team_deploy.sh
@@ -22,6 +22,6 @@ PORT=$2
 
 git pull origin dev
 docker pull hngdevops/nextjs-boilerplate:dev
-docker stop hngdevops/nextjs-boilerplate:dev
-docker rm hngdevops/nextjs-boilerplate:dev -f
+docker stop hngdevops/nextjs-boilerplate:dev || true
+docker rm hngdevops/nextjs-boilerplate:dev -f || true
 docker run -d --name "$TEAM_NAME" -p "$PORT:3000" hngdevops/nextjs-boilerplate:dev
