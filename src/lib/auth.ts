768d1d8d58e2be323eeff30e0a1c56a10747d9cd
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-07-31 02:44:53 +0100

    chore: default user session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,17 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 
 import authConfig from "~/config/auth.config";
+
+interface User {
+  id: string;
+  email: string;
+  fullname?: string;
+  avatar_url: string;
+  first_name?: string;
+  last_name: string;
+  role: string;
+  access_token: string;
+}
 
 export const {
   handlers: { GET, POST },
@@ -10,3 +21,18 @@ export const {
   ...authConfig,
   secret: process.env.AUTH_SECRET,
 });
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: User["id"];
+      name: User["fullname"];
+      first_name: User["first_name"];
+      last_name: User["last_name"];
+      email: User["email"];
+      image: User["avatar_url"];
+      role: User["role"];
+      accessToken: User["access_token"];
+    } & DefaultSession["user"];
+  }
+}
