00e2c8fdc86141e48c7b73741496457742955257
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-07-31 03:25:15 +0100

    chore: authentication actions

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,17 +1,62 @@
 "use server";
 
-import { createSession, deleteSession } from "~/lib/services/session";
+import axios from "axios";
+import * as z from "zod";
 
-interface Properties {
-  email: string;
-  password: string;
-}
+import { LoginSchema } from "~/schemas";
 
-export const loginUser = async (data: Properties) => {
-  const { email } = data;
-  await createSession(email);
+const apiUrl = process.env.API_URL;
+
+export const loginUser = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return {
+      error: "Login Failed. Please check your email and password.",
+    };
+  }
+  try {
+    const response = await axios.post(
+      `${apiUrl}/api/v1/auth/login`,
+      validatedFields.data,
+    );
+    return {
+      data: response.data,
+    };
+  } catch (error) {
+    return axios.isAxiosError(error) && error.response
+      ? {
+          error: error.response.data.message || "Login failed.",
+          status: error.response.status,
+        }
+      : {
+          error: "An unexpected error occurred.",
+        };
+  }
 };
 
-export async function deleteUserCookie() {
-  deleteSession();
-}
+export const nextlogin = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return {
+      error: "Login Failed. Please check your email and password.",
+    };
+  }
+  try {
+    const response = await axios.post(
+      `${apiUrl}/api/v1/auth/login`,
+      validatedFields.data,
+    );
+    return {
+      data: response.data,
+    };
+  } catch (error) {
+    return axios.isAxiosError(error) && error.response
+      ? {
+          error: error.response.data.message || "Login failed.",
+          status: error.response.status,
+        }
+      : {
+          error: "An unexpected error occurred.",
+        };
+  }
+};
---
+++ src/schemas/index.ts
@@ -0,0 +1,29 @@
+import * as z from "zod";
+
+export const RegisterSchema = z.object({
+  first_name: z.string().min(3, {
+    message: "first name must be at least 2 characters.",
+  }),
+  last_name: z.string().min(3, {
+    message: "first name must be at least 2 characters.",
+  }),
+  email: z.string().email(),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
+
+export const LoginSchema = z.object({
+  email: z.string().min(3, {
+    message: "Feild is required",
+  }),
+  password: z.string().min(3, {
+    message: "Password is required",
+  }),
+});
+
+export const OtpSchema = z.object({
+  otp: z.string().optional(),
+  token: z.string().optional(),
+  email: z.string().email().optional(),
+});
