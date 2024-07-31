1be597a854e7a5ed3f9f0e32d62616f1759bddf2
Vxrcel <skyeparagon@gmail.com>
2024-07-31 22:04:30 +0100

    blur image

--- src/components/miscellaneous/blur-image.tsx
+++ src/components/miscellaneous/blur-image.tsx
@@ -13,14 +13,14 @@ export default function BlurImage(properties: ComponentProps<typeof Image>) {
       {...properties}
       alt={properties.alt}
       className={cn(
-        "duration-700 ease-in-out",
-        isLoading ? "scale-105 blur-lg" : "scale-100 blur-0",
+        "duration-500 ease-in-out",
+        isLoading ? "blur-lg" : "blur-0",
         properties.className,
       )}
       onLoad={() => {
         setTimeout(() => {
           setLoading(false);
-        }, 500);
+        }, 300);
       }}
     />
   );
