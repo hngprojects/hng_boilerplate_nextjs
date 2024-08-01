a26f168a6a4296e75675e18bec373983e700e257
obanijesuadufe8 <obanijesuadufe8@gmail.com>
2024-08-01 16:16:53 +0000

    fix: lint errors

--- src/app/(landing-routes)/blog/data/mock.ts
+++ src/app/(landing-routes)/blog/data/mock.ts
@@ -14,13 +14,8 @@ import {
 
 export const blogPosts = [
   {
-<<<<<<< HEAD
     id: "1",
     title: "The Power of Networking: How to Build Meaningful Connections",
-=======
-    id: 1,
-    title: "5 Mistakes That Kill Your Start-up Before It Takes off",
->>>>>>> e2df7f70 (feat: develop the blog page)
     date: "Jul 12, 2024",
     readTime: "5",
     category: "Business",
@@ -28,65 +23,40 @@ export const blogPosts = [
     labelClassName: "bg-primary",
   },
   {
-<<<<<<< HEAD
     id: "2",
     title: "The Global Impact of Climate Change: A Look at the Evidence",
-=======
-    id: 2,
-    title: "5 Mistakes That Kill Your Start-up Before It Takes off",
->>>>>>> e2df7f70 (feat: develop the blog page)
     date: "Jul 12, 2024",
     readTime: "5",
     category: "World News",
     image: blogCard2,
   },
   {
-<<<<<<< HEAD
     id: "3",
     title: "5 Easy and Delicious Recipes for Busy Weeknights",
-=======
-    id: 3,
-    title: "5 Mistakes That Kill Your Start-up Before It Takes off",
->>>>>>> e2df7f70 (feat: develop the blog page)
     date: "Jul 12, 2024",
     readTime: "5",
     category: "Food",
     image: blogCard3,
   },
   {
-<<<<<<< HEAD
     id: "4",
     title: "5 Simple Habits to Improve Your Mental Wellbeing",
-=======
-    id: 4,
-    title: "5 Mistakes That Kill Your Start-up Before It Takes off",
->>>>>>> e2df7f70 (feat: develop the blog page)
     date: "Jul 12, 2024",
     readTime: "5",
     category: "Lifestyle",
     image: blogCard4,
   },
   {
-<<<<<<< HEAD
     id: "5",
     title: "The Ultimate Guide to Dressing Stylishly with Fewer Clothes",
-=======
-    id: 5,
-    title: "5 Mistakes That Kill Your Start-up Before It Takes off",
->>>>>>> e2df7f70 (feat: develop the blog page)
     date: "Jul 12, 2024",
     readTime: "5",
     category: "Fashion",
     image: blogCard5,
   },
   {
-<<<<<<< HEAD
     id: "6",
     title: "The Future of Travel: What Will the World Look Like in 2030?",
-=======
-    id: 6,
-    title: "5 Mistakes That Kill Your Start-up Before It Takes off",
->>>>>>> e2df7f70 (feat: develop the blog page)
     date: "Jul 12, 2024",
     readTime: "5",
     category: "World News",
