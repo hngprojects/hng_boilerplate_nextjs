24f4f639523ff1f2767ec80576c5c773f3d805cd
Prudent Bird <prudentbird@gmail.com>
2024-08-01 04:26:26 +0100

    chore: resolved merge conflicts

--- src/app/dashboard/(admin)/layout.tsx
+++ src/app/dashboard/(admin)/layout.tsx
@@ -11,6 +11,7 @@ export default function AdminLayout({
   return (
     <div className="min-h-screen w-full">
       <DashboardNavbar />
+      <Providers />
       <div className="">
         <main className="mt-12 p-3 pt-6 md:p-5 md:pt-8">
           <Suspense>{children}</Suspense>
