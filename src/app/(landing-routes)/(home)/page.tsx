2729affdcea48c880f8a688918342eec71ba3b2d
Destiny Saturday <destiny@destinedcodes.com>
2024-08-03 13:18:39 +0100

    Update page.tsx
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- src/app/(landing-routes)/(home)/page.tsx
+++ src/app/(landing-routes)/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <UserSection />
       <HowItWorks />
-      <Testimonials />
+{/*       <Testimonials /> */}
       <PerfectFit />
     </>
   );
