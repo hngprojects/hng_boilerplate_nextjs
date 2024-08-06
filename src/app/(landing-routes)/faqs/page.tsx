fe2ebc022ceec004ed1e173e1dd2f6668b1382b9
Ayodejioladimeji <layobright11@gmail.com>
2024-08-06 17:58:19 +0100

    Enhancement: Refactored FAQs page

--- src/app/(landing-routes)/faqs/page.tsx
+++ src/app/(landing-routes)/faqs/page.tsx
@@ -1,26 +1,57 @@
-import React from "react";
+"use client";
 
-import Faqs from "~/components/common/Faqs";
-import AdditionalInquiriesForm from "./inquiries-form/AdditionalInquiriesForm";
-import Heading from "~/components/layouts/heading";
-import { faqData } from "~/constants/faqsdata";
+import React, { useEffect, useState } from "react";
+
+import { getFaqs } from "~/actions/externalPages";
 import FaqAccordion from "~/components/layouts/accordion/FaqsAccordion";
+import Heading from "~/components/layouts/heading";
+import FaqSkeleton from "~/components/skeleton/faqskeleton";
+import AdditionalInquiriesForm from "./inquiries-form/AdditionalInquiriesForm";
 
 const faq: React.FC = () => {
+  const [faqs, setFaqs] = useState<any>([]);
+  const [loading, setLoading] = useState(true);
+
+  // get faqs
+  useEffect(() => {
+    const fetchFaqs = async () => {
+      const result = await getFaqs();
+      if(result?.status === 200 || result?.status === 201){
+        setFaqs(result?.data?.data);
+      }
+      
+      setLoading(false)
+    };
+    fetchFaqs();
+  }, []);
+
   return (
     <div className="bg-white">
       <main className="mx-auto max-w-7xl bg-white px-5 py-10 sm:bg-transparent md:px-10 lg:px-10 xl:px-10">
-        <Heading tag="FAQS" title="Frequently {{asked}} questions" content="Questions you might ask about our product"/>
-        
-        <div className="mt-4 mb-40 max-w-xl mx-auto">
-           <FaqAccordion
-              faqs={faqData}
-              containerClassName="sm:w-full px-4 py-1 bg-white"
-              data-testid="faq-accordion"
-            />
+        <Heading
+          tag="FAQS"
+          title="Frequently {{asked}} questions"
+          content="Questions you might ask about our product"
+        />
+
+        <div className="mx-auto mb-40 mt-4 max-w-xl">
+          {loading ? (
+            <FaqSkeleton />
+          ) : (
+            <>
+              {faqs?.length === 0 ? (
+                <p className="py-20 text-center">No results found.</p>
+              ) : (
+                <FaqAccordion
+                  faqs={faqs}
+                  containerClassName="sm:w-full px-4 py-1 bg-white"
+                  data-testid="faq-accordion"
+                />
+              )}
+            </>
+          )}
         </div>
 
-        
         <AdditionalInquiriesForm />
       </main>
     </div>
---
+++ src/components/skeleton/faqskeleton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Skeleton } from "~/components/ui/skeleton";
+
+//
+
+const FaqSkeleton = () => {
+  return Array.from({ length: 4 }).map((_, index) => (
+    <div
+      className="w-full rounded-xl border border-border px-2 py-2 mb-3"
+      key={index}
+    >
+      <Skeleton
+        className="h-[40px] w-full rounded-md"
+        data-testid="skeleton"
+      />
+    </div>
+  ));
+};
+
+export default FaqSkeleton;
