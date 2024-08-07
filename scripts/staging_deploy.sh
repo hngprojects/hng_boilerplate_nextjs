acd864003ed993a58e090a3cd48cf7311ba451b9
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2024-08-07 14:19:10 +0100

    Update staging_deploy.sh
    Signed-off-by: Osinachi Chukwujama  <Osinachi.Chukwujama@gmail.com>

--- scripts/staging_deploy.sh
+++ scripts/staging_deploy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+
+echo "something something something"
+
 cd "$(git rev-parse --show-toplevel)"
 git pull origin staging
 docker compose --project-name staging-nextjs -f docker/staging/docker-compose.yml up -d
