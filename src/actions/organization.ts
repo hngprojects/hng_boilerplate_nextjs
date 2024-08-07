3a14a6f4eea451f1ac3e7f91d2ea8fc398175ee1
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-08-07 17:57:15 +0100

    feat: subscription

--- src/actions/organization.ts
+++ src/actions/organization.ts
@@ -3,50 +3,6 @@
 import axios from "axios";
 
 import { auth } from "~/lib/auth";
-import { User } from "~/types";
-
-export enum StockStatusType {
-  IN_STOCK = "in stock",
-  OUT_STOCK = "out of stock",
-  LOW_STOCK = "low on stock",
-}
-
-export enum ProductSizeType {
-  SMALL = "Small",
-  STANDARD = "Standard",
-  LARGE = "Large",
-}
-
-export interface Product {
-  id: string;
-  name: string;
-  description?: string;
-  category?: string;
-  image?: string;
-  price: number;
-  quantity: number;
-  is_deleted?: boolean;
-  size: ProductSizeType;
-  stock_status: StockStatusType;
-  org: Organisation;
-}
-
-export interface Organisation {
-  id: string;
-  name: string;
-  description: string;
-  email: string;
-  industry: string;
-  type: string;
-  country: string;
-  address: string;
-  owner: User;
-  state: string;
-  creator: User;
-  isDeleted: boolean;
-  products: Product[];
-  role: "admin" | "manager" | "user" | "custom" | string;
-}
 
 const apiUrl = process.env.API_URL;
 
