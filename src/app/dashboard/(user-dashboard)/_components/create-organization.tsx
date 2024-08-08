197a5e38f248ab1c94dc9407f4b3d967fb4c2fab
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-08-08 18:03:38 +0100

    fix : auth fix

---
+++ src/app/dashboard/(user-dashboard)/_components/create-organization.tsx
@@ -0,0 +1,307 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { createOrg } from "~/actions/organization";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { useToast } from "~/components/ui/use-toast";
+import { organizationSchema } from "~/schemas";
+import { countries } from "./county";
+
+interface ModalProperties {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+export const CreateOrganization = ({ isOpen, setIsOpen }: ModalProperties) => {
+  const [selectedCountry, setSelectedCountry] = useState<string | undefined>();
+  const [states, setStates] = useState<string[]>([]);
+  const [isLoading, startTransition] = useTransition();
+  const { toast } = useToast();
+
+  const handleCountryChange = (value: string) => {
+    setSelectedCountry(value);
+    const country = countries.find((c) => c.name === value);
+    if (country) {
+      setStates(country.states);
+    }
+  };
+  const form = useForm<z.infer<typeof organizationSchema>>({
+    resolver: zodResolver(organizationSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      description: "",
+      industry: "",
+      type: "",
+      country: "",
+      state: "",
+      address: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof organizationSchema>) => {
+    startTransition(async () => {
+      await createOrg(values).then((data) => {
+        if (data.status === 201) {
+          setIsOpen(false);
+        }
+        toast({
+          title:
+            data.status === 201
+              ? "organization created successfully"
+              : "organization not created",
+          description:
+            data.status === 201
+              ? "organization created successfully"
+              : data.error,
+        });
+      });
+    });
+  };
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="gap-2">
+            <div className="flex flex-col gap-2 md:flex-row">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="example@domain.com"
+                        {...field}
+                        className="w-full"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Enter a valid email address.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Company Name"
+                        {...field}
+                        className="w-full"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Enter the name of the company.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:flex-row">
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Description"
+                        {...field}
+                        className="w-full"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Provide a brief description of the company.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="industry"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Industry</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Industry"
+                        {...field}
+                        className="w-full"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Specify the industry the company operates in.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:flex-row">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Type</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Type"
+                        {...field}
+                        className="w-full"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Indicate the type of organization.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="country"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={(value) => {
+                          field.onChange(value);
+                          handleCountryChange(value);
+                        }}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a country" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {countries.map((country) => (
+                            <SelectItem key={country.name} value={country.name}>
+                              {country.name}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormDescription>
+                      Choose the country where the organization is based.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:flex-row">
+              <FormField
+                control={form.control}
+                name="state"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>State</FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        disabled={!selectedCountry}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a state" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {states.map((state) => (
+                            <SelectItem key={state} value={state}>
+                              {state}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormDescription>
+                      Select the state or province.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Address"
+                        {...field}
+                        className="w-full"
+                        disabled={isLoading}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Enter the address of the organization.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit">Create</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/app/dashboard/(user-dashboard)/_components/layout/navbar/organisation-switcher.tsx
+++ src/app/dashboard/(user-dashboard)/_components/layout/navbar/organisation-switcher.tsx
@@ -1,6 +1,6 @@
 "use client ";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
@@ -13,6 +13,7 @@ import {
 } from "~/components/ui/select";
 import { useOrgContext } from "~/contexts/orgContext";
 import { useLocalStorage } from "~/hooks/use-local-storage";
+import { CreateOrganization } from "../../create-organization";
 
 export const OrganisationSwitcher = () => {
   const [currentId, setCurrentOrgId] = useLocalStorage<string | undefined>(
@@ -31,36 +32,46 @@ export const OrganisationSwitcher = () => {
     organizations.length > 0
       ? organizations.find((org) => org.id === currentId)
       : undefined;
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Select defaultValue={currentId} onValueChange={setCurrentOrgId}>
-      <SelectTrigger
-        aria-label="Select organisation"
-        className="flex items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0"
-      >
-        <SelectValue placeholder="Select an organisation">
-          <span className="ml-2">
-            {currentOrg ? currentOrg.name : "Select an organisation"}
-          </span>
-        </SelectValue>
-      </SelectTrigger>
-      <SelectContent>
-        {organizations.map((org) => (
-          <SelectItem key={org.id} value={org.id}>
-            <div className="flex items-center gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
-              <Avatar className="size-4 sm:size-3">
-                <AvatarImage src={""} />
-                <AvatarFallback className="bg-primary/30 uppercase">
-                  {org?.name?.charAt(0)}
-                </AvatarFallback>
-              </Avatar>{" "}
-              {org.name}
-            </div>
-          </SelectItem>
-        ))}
-        <Button variant="outline" className="w-full">
-          Create Organisation
-        </Button>
-      </SelectContent>
-    </Select>
+    <>
+      <CreateOrganization isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Select defaultValue={currentId} onValueChange={setCurrentOrgId}>
+        <SelectTrigger
+          aria-label="Select organisation"
+          className="flex items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0"
+        >
+          <SelectValue placeholder="Select an organisation">
+            <span className="ml-2">
+              {currentOrg ? currentOrg.name : "Select an organisation"}
+            </span>
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          {organizations.map((org) => (
+            <SelectItem key={org.id} value={org.id}>
+              <div className="flex items-center gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
+                <Avatar className="size-4 sm:size-3">
+                  <AvatarImage src={""} />
+                  <AvatarFallback className="bg-primary/30 uppercase">
+                    {org?.name?.charAt(0)}
+                  </AvatarFallback>
+                </Avatar>{" "}
+                {org.name}
+              </div>
+            </SelectItem>
+          ))}
+          <Button
+            variant="outline"
+            className="w-full"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            Create Organisation
+          </Button>
+        </SelectContent>
+      </Select>
+    </>
   );
 };
