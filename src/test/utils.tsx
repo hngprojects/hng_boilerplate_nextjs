994263a942fddc3ad57bbc5e557da0d53634ffac
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-08-14 14:15:56 +0100

    feat/multiple language support (en, es, fr)

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,1 +1,14 @@
+import React, { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+
+const renderWithIntl = (ui: ReactNode, { locale = 'en', messages = {} } = {}) => {
+    return render(
+        <NextIntlClientProvider locale={locale} messages={messages}>
+            {ui}
+        </NextIntlClientProvider>
+    );
+};
+
 export * from "@testing-library/react";
+export { renderWithIntl }
