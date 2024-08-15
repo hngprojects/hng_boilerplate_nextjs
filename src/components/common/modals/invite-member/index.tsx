8c3cf1b1823163b94660c2684c1ae3a43e54b15b
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-08-15 18:06:54 +0100

    feat: implement functions for inviting members to organization

--- src/components/common/modals/invite-member/index.tsx
+++ src/components/common/modals/invite-member/index.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable no-console */
 "use client";
 
 import axios from "axios";
 import { Link2Icon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import { fetchOrganizations, InviteMembers } from "~/actions/organization";
 import CustomButton from "~/components/common/common-button/common-button";
 import {
   Dialog,
@@ -22,49 +20,18 @@ import {
   SelectTrigger,
   SelectValue,
 } from "~/components/ui/select";
-import { useToast } from "~/components/ui/use-toast";
-// Importing apiUrl from the config file
-import { auth } from "~/lib/auth";
-import { apiUrl } from "./config"; // Importing apiUrl from the config file
 
 interface ModalProperties {
   show: boolean;
   onClose: () => void;
 }
 
-interface Organization {
-  id: string;
-  name: string;
-}
-
 const InviteMemberModal: React.FC<ModalProperties> = ({ show, onClose }) => {
   const [emails, setEmails] = useState("");
-  const [organization, setOrganization] = useState<string | null>("null");
-  const [organizations, setOrganizations] = useState<Organization[]>([]);
-
+  const [organization, setOrganization] = useState("");
   const [inviteLink, setInviteLink] = useState("");
   const [linkGenerated, setLinkGenerated] = useState(false);
   const [error, setError] = useState("");
-
-  // const apiUrl = useApiUrl();
-  const { toast } = useToast(); // Initialize the toast hook
-  useEffect(() => {
-    const loadOrganizations = async () => {
-      const { data, error } = await fetchOrganizations();
-      if (data) {
-        setOrganizations(data);
-      } else {
-        console.error("Error fetching organizations:", error);
-        toast({
-          title: "Error",
-          description: "Failed to load organizations.",
-          variant: "destructive",
-        });
-      }
-    };
-
-    loadOrganizations();
-  }, [apiUrl]); //
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmails(event.target.value);
@@ -75,141 +42,105 @@ const InviteMemberModal: React.FC<ModalProperties> = ({ show, onClose }) => {
   };
 
   const handleSubmit = async () => {
-    if (!emails || !organization) {
-      setError("Emails and Organization ID are required.");
-      return;
-    }
-
-    const result = await InviteMembers(emails, organization); // Call the function
-
-    if (result.data) {
-      toast({
-        title: "Success",
-        description: "Invites sent successfully!",
-        variant: "default",
+    try {
+      const response = await axios.post("/api/v1/invite/create", {
+        emails: emails.split(",").map((email) => email.trim()),
+        organization,
       });
-      onClose(); // Close the modal on success
-    } else {
-      setError(result.error || "Failed to send invites.");
-      toast({
-        title: "Error",
-        description: result.error || "Failed to send invites.",
-        variant: "destructive",
-      });
+      if (response.status === 200) {
+        alert("Invites sent successfully!");
+        onClose();
+      }
+    } catch {
+      setError("Failed to send invites.");
     }
   };
 
   const handleInviteWithLink = async () => {
     try {
-      const session = await auth();
-
-      const response = await axios.post(
-        `${apiUrl}/api/v1/invite/create`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${session?.access_token}`,
-          },
-        },
-      );
-
+      const response = await axios.post("/api/v1/invite/create");
       if (response.status === 200) {
         setInviteLink(response.data.invite_link);
         setLinkGenerated(true);
         navigator.clipboard.writeText(response.data.invite_link);
-        toast({
-          title: "Success",
-          description: "Invite link copied to clipboard!",
-          variant: "default",
-        });
-      } else {
-        setError("Failed to generate invite link.");
-        toast({
-          title: "Error",
-          description: "Failed to generate invite link.",
-          variant: "destructive",
-        });
+        alert("Invite link copied to clipboard!");
       }
     } catch {
       setError("Failed to generate invite link.");
-      toast({
-        title: "Error",
-        description: "Failed to generate invite link.",
-        variant: "destructive",
-      });
     }
   };
 
   return (
-    <Dialog open={show} onOpenChange={onClose}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="flex items-center gap-2 border-b border-gray-300">
-            <div className="mt-[-8px] flex h-10 w-10 items-center justify-center rounded-full bg-secondary text-center">
-              KP
-            </div>
-            <h2 className="mb-2 text-left text-lg text-neutral-dark-2">
-              Invite to your Organization
-            </h2>
-          </DialogTitle>
-          <DialogDescription>
-            <div className="mb-7 mt-6">
-              <label className="mb-2 block text-left text-base text-neutral-dark-2">
-                Email
-              </label>
-              <input
-                type="text"
-                placeholder="email@example.com, email2@example.com..."
-                className="w-full rounded-md border border-border px-3 py-2 shadow-sm outline-none focus:border-primary focus:ring-ring"
-                value={emails}
-                onChange={handleEmailChange}
-              />
-            </div>
-            <div>
-              <label className="mb-2 block text-left text-base text-neutral-dark-2">
-                Add to Organization (Optional)
-              </label>
-              <Select onValueChange={handleOrganizationChange}>
-                <SelectTrigger className="bg-white">
-                  <SelectValue placeholder="Select Organization" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    {organizations.map((organization) => (
-                      <SelectItem key={organization.id} value={organization.id}>
-                        {organization.name}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="mt-8 flex items-center justify-between gap-4">
-              <span className="relative flex items-center text-primary">
-                <Link2Icon className="pointer-events-none absolute ml-3" />
-                <CustomButton
-                  variant="subtle"
-                  onClick={handleInviteWithLink}
-                  className="pl-10"
-                >
-                  Invite with link
-                </CustomButton>
-              </span>
-
-              <CustomButton variant="primary" onClick={handleSubmit}>
-                Send Invites
-              </CustomButton>
-            </div>
-            {linkGenerated && (
-              <div className="mt-4 text-sm text-green-500">
-                Invite link: {inviteLink}
+    <>
+      <Dialog open={show} onOpenChange={onClose}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="flex items-center gap-2 border-b border-gray-300">
+              <div className="mt-[-8px] flex h-10 w-10 items-center justify-center rounded-full bg-secondary text-center">
+                KP
               </div>
-            )}
-            {error && <div className="mt-4 text-sm text-red-500">{error}</div>}
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+              <h2 className="mb-2 text-left text-lg text-neutral-dark-2">
+                Invite to your Organization
+              </h2>
+            </DialogTitle>
+            <DialogDescription>
+              <div className="mb-7 mt-6">
+                <label className="mb-2 block text-left text-base text-neutral-dark-2">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="email@example.com, email2@example.com..."
+                  className="w-full rounded-md border border-border px-3 py-2 shadow-sm outline-none focus:border-primary focus:ring-ring"
+                  value={emails}
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div>
+                <label className="mb-2 block text-left text-base text-neutral-dark-2">
+                  Add to Organization (Optional)
+                </label>
+                <Select onValueChange={handleOrganizationChange}>
+                  <SelectTrigger className="bg-white">
+                    <SelectValue placeholder="Select Organization" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="org1">Org 1</SelectItem>
+                      <SelectItem value="org2">Org 2</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="mt-8 flex items-center justify-between gap-4">
+                <span className="relative flex items-center text-primary">
+                  <Link2Icon className="pointer-events-none absolute ml-3" />
+                  <CustomButton
+                    variant="subtle"
+                    onClick={handleInviteWithLink}
+                    className="pl-10"
+                  >
+                    Invite with link
+                  </CustomButton>
+                </span>
+
+                <CustomButton variant="primary" onClick={handleSubmit}>
+                  Send Invites
+                </CustomButton>
+              </div>
+              {linkGenerated && (
+                <div className="mt-4 text-sm text-green-500">
+                  Invite link: {inviteLink}
+                </div>
+              )}
+              {error && (
+                <div className="mt-4 text-sm text-red-500">{error}</div>
+              )}
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
