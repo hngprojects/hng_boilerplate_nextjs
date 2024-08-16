f46ee9601e57f09a09373e851e4e88384fb60131
Dunamis Olukayode <callmestiless@gmail.com>
2024-08-16 12:18:16 +0300

    fix: fix lint errors

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -4,22 +4,6 @@ export const useLocalStorage = <T>(
   key: string,
   initialValue: T,
 ): [T, (value: T) => void] => {
-<<<<<<< HEAD
-  const [storedValue, setStoredValue] = useState<T>(initialValue);
-
-  useEffect(() => {
-    const item = window.localStorage.getItem(key);
-    if (item) {
-      try {
-        setStoredValue(JSON.parse(item));
-      } catch {
-        // console.error("Error parsing JSON from localStorage", error);
-        // Optionally reset to initial value in case of parse error
-        setStoredValue(initialValue);
-      }
-    }
-  }, [key, initialValue]);
-=======
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key);
@@ -29,7 +13,6 @@ export const useLocalStorage = <T>(
       return initialValue;
     }
   });
->>>>>>> origin/dev
 
   const setValue = (value: T) => {
     setStoredValue(value);
