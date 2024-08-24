c3f24240153e9f3205f6a4de2d3cfcea442194d2
deji2ghost <dejiwilliams9@gmail.com>
2024-08-24 02:17:00 +0200

    feat: update navbar on settings page.

--- src/app/dashboard/(admin)/_components/layout/navbar/index.tsx
+++ src/app/dashboard/(admin)/_components/layout/navbar/index.tsx
@@ -74,48 +74,55 @@ const DashboardNavbar = () => {
           <DashboardLogo />
         </div>
         <div className="flex w-full justify-between">
-          <div className="flex h-10 items-center justify-between gap-2 rounded-[6px] border border-border bg-white px-3 text-sm font-normal placeholder:text-sm">
-            <SearchIcon
-              data-testid="search"
-              className="h-4 w-4 text-neutral-dark-2"
-            />
-            <input
-              className="h-full w-full border-none text-neutral-dark-2 outline-none ring-0 placeholder:text-neutral-dark-1"
-              placeholder="Search option..."
-              data-testid="input"
-            />
+          <div className="flex items-center gap-[22px] text-[12px] font-medium text-[#000000]">
+            <p>overview</p>
+            <p>customer</p>
+            <p>products</p>
           </div>
-          <div className="flex items-center justify-between gap-1 md:gap-2 lg:gap-4">
-            <div className="relative flex items-center justify-center">
-              <Popover>
-                <PopoverTrigger>
-                  <BellIcon
-                    data-testid="bell"
-                    className="h-6 w-6 text-neutral-dark-2 transition-colors duration-300 hover:cursor-pointer hover:text-neutral-dark-1"
-                  />
-                </PopoverTrigger>
-                <PopoverContent
-                  data-testid="notificationContent"
-                  align="end"
-                  className="w-fit border-none p-0 shadow-none"
-                >
-                  <UnreadNotificationCard
-                    notificationsPreview={notificationContent}
-                    unreadCount={totalUnreadNotificationCount}
-                    totalNotificationCount={totalNotificationCount}
-                  />
-                </PopoverContent>
-              </Popover>
-              <span className="absolute right-1 top-0 h-[6px] w-[6px] rounded-full bg-error"></span>
-            </div>
-            <div>
-              <HelpCircle
-                data-testid="help"
-                className="h-6 w-6 text-neutral-dark-2 transition-colors duration-300 hover:cursor-pointer hover:text-neutral-dark-1"
+          <div className="flex items-center gap-5">
+            <div className="flex h-10 items-center justify-between gap-2 rounded-[6px] border border-border bg-white px-3 text-sm font-normal placeholder:text-sm">
+              <SearchIcon
+                data-testid="search"
+                className="h-4 w-4 text-neutral-dark-2"
+              />
+              <input
+                className="h-full w-full border-none text-neutral-dark-2 outline-none ring-0 placeholder:text-neutral-dark-1"
+                placeholder="Search option..."
+                data-testid="input"
               />
             </div>
-            <div className="hover:bg-black-1 flex w-full max-w-[64px] cursor-pointer items-center justify-between gap-2">
-              <UserCard />
+            <div className="flex items-center justify-between gap-1 md:gap-2 lg:gap-4">
+              <div className="relative flex items-center justify-center">
+                <Popover>
+                  <PopoverTrigger>
+                    <BellIcon
+                      data-testid="bell"
+                      className="h-6 w-6 text-neutral-dark-2 transition-colors duration-300 hover:cursor-pointer hover:text-neutral-dark-1"
+                    />
+                  </PopoverTrigger>
+                  <PopoverContent
+                    data-testid="notificationContent"
+                    align="end"
+                    className="w-fit border-none p-0 shadow-none"
+                  >
+                    <UnreadNotificationCard
+                      notificationsPreview={notificationContent}
+                      unreadCount={totalUnreadNotificationCount}
+                      totalNotificationCount={totalNotificationCount}
+                    />
+                  </PopoverContent>
+                </Popover>
+                <span className="absolute right-1 top-0 h-[6px] w-[6px] rounded-full bg-error"></span>
+              </div>
+              <div>
+                <HelpCircle
+                  data-testid="help"
+                  className="h-6 w-6 text-neutral-dark-2 transition-colors duration-300 hover:cursor-pointer hover:text-neutral-dark-1"
+                />
+              </div>
+              <div className="hover:bg-black-1 flex w-full max-w-[64px] cursor-pointer items-center justify-between gap-2">
+                <UserCard />
+              </div>
             </div>
           </div>
         </div>
