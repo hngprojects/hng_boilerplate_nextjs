a2a8eceeb67d6de19e11a6c6008f127becea08ee
Prudent Bird <prudentbird@gmail.com>
2024-08-24 19:43:52 +0100

    chore: fixed lint errors

--- src/app/(landing-routes)/contact-us/page.test.tsx
+++ src/app/(landing-routes)/contact-us/page.test.tsx
@@ -1,15 +1,13 @@
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
 import { render } from "~/test/utils";
 import Page from "./page";
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from 'react';
 
 describe("page tests", () => {
-
   const renderWithSession = (component: ReactNode) => {
     return render(
-      <SessionProvider session={null}>
-        {component}
-      </SessionProvider>
+      <SessionProvider session={undefined}>{component}</SessionProvider>,
     );
   };
   it("should render correctly", () => {
