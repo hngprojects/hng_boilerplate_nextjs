9e347df9cf46b771c3c5f9c748fa50574babe054
Feng-09 <Tifeng9@gmail.com>
2024-08-24 11:44:22 +0100

    Fix: fix typescript error

--- src/app/(landing-routes)/blog/[id]/RelatedArticle.tsx
+++ src/app/(landing-routes)/blog/[id]/RelatedArticle.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/navigation";
+// import { blogPosts } from "../data/mock";
+import { useEffect, useState } from "react";
 
 import BlogCard from "~/components/layouts/BlogCards";
-import { blogPosts } from "../data/mock";
 
 export type New = {
   id: number;
@@ -10,29 +11,67 @@ export type New = {
 
 const RelatedArticle = () => {
   const router = useRouter();
+  const [data, setData] = useState<
+    {
+      id: string;
+      title: string;
+      date: string;
+      readTime: string;
+      category: string;
+      image: string;
+      labelClassName: string;
+    }[]
+  >([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://staging.api-csharp.boilerplate.hng.tech/api/v1/blogs`,
+        );
+        const result = await response.json();
+        setData(result.data);
+      } catch {
+        // console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="">
         <h1 className="font-inter text-center text-xl font-bold leading-[29.05px] text-[#525252] sm:text-2xl md:text-left">
           Related Articles
         </h1>
-        <div className="mt-6 grid gap-6 sm:mt-8 sm:gap-8">
-          {blogPosts.slice(0, 3).map((post, index) => (
-            <BlogCard
-              key={index}
-              title={post.title}
-              date={post.date}
-              readTime={post.readTime}
-              category={post.category}
-              image={post.image}
-              labelClassName={post.labelClassName}
-              onClick={() => {
-                localStorage.setItem("currentBlogPost", JSON.stringify(post));
-                router.push(`/blog/${post.id}`);
-              }}
-            />
-          ))}
-        </div>
+        {isLoading ? (
+          <div className="animate-pulse text-2xl font-bold">
+            Loading Related...
+          </div>
+        ) : (
+          <div className="mt-6 grid gap-6 sm:mt-8 sm:gap-8">
+            {data?.slice(0, 3).map((post, index) => (
+              <BlogCard
+                key={index}
+                title={post.title}
+                date={post.date}
+                readTime={post.readTime}
+                category={post.category}
+                image={post.image}
+                labelClassName={post.labelClassName}
+                onClick={() => {
+                  localStorage.setItem("currentBlogPost", JSON.stringify(post));
+                  router.push(`/blog/${post.id}`);
+                }}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
