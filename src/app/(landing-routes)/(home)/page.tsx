289c520d1c0e7031d3f067b5c23c8f8c08cc939a
Mary <101870828+Chioma227@users.noreply.github.com>
2024-08-24 21:07:13 +0100

    feat: add marketing ad video

--- src/app/(landing-routes)/(home)/page.tsx
+++ src/app/(landing-routes)/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "~/components/layouts/homepage/Hero";
 import HowItWorks from "~/components/layouts/homepage/HowItWorks";
+import MarketingAd from "~/components/layouts/homepage/MarketingAd";
 import PerfectFit from "~/components/layouts/homepage/PerfectFit";
 import Testimonials from "~/components/layouts/homepage/Testimonials";
 import UserSection from "~/components/layouts/homepage/UserSection";
@@ -13,6 +14,7 @@ export default function Home() {
       <Video />
       <HowItWorks />
       <Testimonials />
+      <MarketingAd />
       <PerfectFit />
     </>
   );
