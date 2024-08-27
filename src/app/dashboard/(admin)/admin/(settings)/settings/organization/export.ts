25313a39e1d9186842fcaed35947f3bd992d32c5
Joshua Oloton <olotonjoshua@gmail.com>
2024-08-27 17:03:27 +0100

    fix: correct invalid export csv url

--- src/app/dashboard/(admin)/admin/(settings)/settings/organization/export.ts
+++ src/app/dashboard/(admin)/admin/(settings)/settings/organization/export.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 import { getApiUrl } from "~/actions/getApiUrl";
+import { useSession } from "next-auth/react";
 
 export const exportMembersEndpoint = async (format: string = "csv") => {
   const apiUrl = await getApiUrl();
+  const { data: session } = useSession();
+  if (!session?.currentOrgId) return;
   try {
     const response = await axios.get(
-      `${apiUrl}/api/v1/organisation/members/export`,
+      `${apiUrl}/api/v1/organisations/${session?.currentOrgId}/users/export`,
       {
         params: {
           format: format,
