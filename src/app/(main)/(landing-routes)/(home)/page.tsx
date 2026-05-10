a5c762cc8f85f4417600a53b0da2a9c57349dbe6
JasmondWorks <obafemilared@gmail.com>
2026-05-10 05:37:00 +0100

    chore: Uncommented the other sections on the landing page

--- src/app/(main)/(landing-routes)/(home)/page.tsx
+++ src/app/(main)/(landing-routes)/(home)/page.tsx
@@ -1,21 +1,21 @@
 import FAQs from '~/components/homepage/faqs/Faqs'
-// import Hero from '~/components/homepage/Hero'
-// import HowItWorks from '~/components/homepage/HowItWorks'
-// import PerfectFit from '~/components/homepage/PerfectFit'
+import Hero from '~/components/homepage/Hero'
+import HowItWorks from '~/components/homepage/HowItWorks'
+import PerfectFit from '~/components/homepage/PerfectFit'
 import Testimonials from '~/components/homepage/Testimonials'
-// import UserSection from '~/components/homepage/UserSection'
-// import Video from '~/components/homepage/Video'
+import UserSection from '~/components/homepage/UserSection'
+import Video from '~/components/homepage/Video'
 
 export default function Home() {
   return (
     <>
-      {/* <Hero /> */}
-      {/* <UserSection /> */}
-      {/* <Video /> */}
-      {/* <HowItWorks /> */}
+      <Hero />
+      <UserSection />
+      <Video />
+      <HowItWorks />
       <Testimonials />
       <FAQs />
-      {/* <PerfectFit /> */}
+      <PerfectFit />
     </>
   )
 }
